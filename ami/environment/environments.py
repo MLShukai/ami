575933ad42ad65674c28d74684889f56dbb042eb
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-12-29 19:40:52 +0900

    ADD SensorActuatorEnv

--- ami/environment/environments.py
+++ ami/environment/environments.py
@@ -3,6 +3,9 @@ classes."""
 from abc import ABC, abstractmethod
 from typing import Any
 
+from .actuators import BaseActuator
+from .sensors import BaseSensor
+
 
 class BaseEnvironment(ABC):
     """Abstract base environment class for interacting with real
@@ -24,10 +27,32 @@ class BaseEnvironment(ABC):
             action: The action for the real environment.
         """
 
-    def setup(self):
+    def setup(self) -> None:
         """Called at the start of interaction with the agent."""
         pass
 
-    def teardown(self):
+    def teardown(self) -> None:
         """Called at the end of interaction with the agent."""
         pass
+
+
+class SensorActuatorEnv(BaseEnvironment):
+    """The environment class that has sensor and actuator."""
+
+    def __init__(self, sensor: BaseSensor, actuator: BaseActuator) -> None:
+        self.sensor = sensor
+        self.actuator = actuator
+
+    def observe(self) -> Any:
+        return self.sensor.read()
+
+    def affect(self, action: Any) -> None:
+        self.actuator.operate(action)
+
+    def setup(self) -> None:
+        self.sensor.setup()
+        self.actuator.setup()
+
+    def teardown(self) -> None:
+        self.sensor.teardown()
+        self.actuator.teardown()
