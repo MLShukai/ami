86cd0238dd9376b1be73acbf0ce6b27719ab40cf
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-12-29 18:09:03 +0900

    ADD BaseActuator and ActuatorWrapper

---
+++ ami/environment/actuators.py
@@ -0,0 +1,57 @@
+"""This file contains the abstract base actuator, actuator wrappers, and
+implemented actuator classes."""
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class BaseActuator(ABC):
+    """Abstract base actuator class for affecting actiion to the real
+    environment."""
+
+    @abstractmethod
+    def operate(self, action: Any) -> None:
+        """Operates the real actuator by `action`.
+
+        Args:
+            action: The command for actuator.
+        """
+        raise NotImplementedError
+
+    def setup(self) -> None:
+        """Called at the start of interaction with the agent."""
+        pass
+
+    def teardown(self) -> None:
+        """Called at the end of interaction with the agent."""
+        pass
+
+
+class ActuatorWrapper(BaseActuator):
+    """Wraps the actuator class for modifying the action.
+
+    You must override :meth:`wrap_action` method for wrapping action.
+    """
+
+    def __init__(self, actuator: BaseActuator, *args: Any, **kwds: Any) -> None:
+        self._actuator = actuator
+
+    @abstractmethod
+    def wrap_action(self, action: Any) -> Any:
+        """Wraps the action and return it.
+
+        Args:
+            action: The action from the agent, controller, etc.
+
+        Returns:
+            action: The modified action.
+        """
+        raise NotImplementedError
+
+    def operate(self, action: Any) -> None:
+        return self._actuator.operate(self.wrap_action(action))
+
+    def setup(self) -> None:
+        return self._actuator.setup()
+
+    def teardown(self) -> None:
+        return self._actuator.teardown()
