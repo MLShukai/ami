5392573b01d18818722d617a9bb6484c1c69c5a4
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-01-15 14:31:18 +0900

    Remove Collectors/Users from utils.py

--- tests/data/test_utils.py
+++ tests/data/test_utils.py
@@ -1,65 +1,13 @@
 import pytest
 import torch
 
-from ami.data import DataKeys, StepData
-from ami.data.utils import (
-    DataCollector,
-    DataCollectorsAggregation,
-    DataUser,
-    DataUsersAggregation,
-)
+from ami.data.step_data import DataKeys, StepData
+from ami.data.utils import DataCollector, DataCollectorsDict, DataUsersDict
 
 from .test_data_buffers import DataBufferImpl
 
 
-class TestDataCollectorAndUser:
-    @pytest.fixture
-    def collector(self) -> DataCollector:
-        return DataCollector(DataBufferImpl())
-
-    @pytest.fixture
-    def user(self, collector: DataCollector) -> DataUser:
-        return DataUser(collector)
-
-    @pytest.fixture
-    def step_data(self) -> StepData:
-        return StepData({DataKeys.OBSERVATION: torch.randn(10)})
-
-    # --- Testing Collector ---
-    def test_collect(self, collector: DataCollector, step_data: StepData) -> None:
-
-        collector.collect(step_data)
-
-    def test_move_data(self, collector: DataCollector) -> None:
-
-        buffer = collector._buffer
-        moved_buffer = collector.move_data()
-        assert buffer is moved_buffer
-        assert buffer is not collector._buffer
-
-    # --- Testing User ---
-    def test_get_new_dataset(self, collector: DataCollector, user: DataUser, step_data: StepData) -> None:
-        collector.collect(step_data)
-
-        dataset = user.get_new_dataset()
-        assert torch.equal(dataset[:][0], step_data[DataKeys.OBSERVATION].unsqueeze(0))
-
-        collector.collect(step_data)
-        dataset = user.get_new_dataset()
-        assert torch.equal(dataset[:][0], torch.stack([step_data[DataKeys.OBSERVATION]] * 2))
-
-    def test_clear(self, collector: DataCollector, user: DataUser, step_data: StepData) -> None:
-        collector.collect(step_data)
-
-        collector_buffer = collector._buffer
-        user_buffer = user._buffer
-        user.clear()
-
-        assert collector_buffer is not collector._buffer
-        assert user_buffer is not user._buffer
-
-
-class TestDataCollectorsAggregation:
+class TestDataCollectorsDict:
     @pytest.fixture
     def buffer(self) -> DataBufferImpl:
         return DataBufferImpl()
@@ -73,15 +21,15 @@ class TestDataCollectorsAggregation:
         return StepData({DataKeys.OBSERVATION: torch.randn(10)})
 
     @pytest.fixture
-    def collectors_aggregation(self, collector: DataCollector) -> DataCollectorsAggregation:
-        return DataCollectorsAggregation(a=collector)
+    def collectors_dict(self, collector: DataCollector) -> DataCollectorsDict:
+        return DataCollectorsDict(a=collector)
 
-    def test_collect(self, collectors_aggregation: DataCollectorsAggregation, step_data: StepData) -> None:
-        collectors_aggregation.collect(step_data)
+    def test_collect(self, collectors_dict: DataCollectorsDict, step_data: StepData) -> None:
+        collectors_dict.collect(step_data)
 
     def test_from_buffers(self, buffer: DataBufferImpl) -> None:
-        collectors = DataCollectorsAggregation.from_data_buffers(b=buffer)
+        collectors = DataCollectorsDict.from_data_buffers(b=buffer)
         assert isinstance(collectors["b"], DataCollector)
 
-    def test_get_data_users(self, collectors_aggregation: DataCollectorsAggregation) -> None:
-        assert isinstance(collectors_aggregation.get_data_users(), DataUsersAggregation)
+    def test_get_data_users(self, collectors_dict: DataCollectorsDict) -> None:
+        assert isinstance(collectors_dict.get_data_users(), DataUsersDict)
