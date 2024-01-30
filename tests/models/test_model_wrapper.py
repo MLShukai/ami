f1f89bc48984d86df60124bcc550894c66ef70bf
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-01-30 17:07:30 +0900

    ADD model wrapper class

---
+++ tests/models/test_model_wrapper.py
@@ -0,0 +1,55 @@
+import pytest
+import torch
+import torch.nn as nn
+
+from ami.models.model_wrapper import InferenceWrapper, ModelWrapper
+from tests.helpers import ModelMultiplyP, skip_if_gpu_is_not_available
+
+
+class TestWrappers:
+    def test_device_cpu(self):
+        device = torch.device("cpu")
+        m = ModelWrapper(ModelMultiplyP(), device, True)
+
+        assert m.device == device
+        m.to_default_device()
+        assert m.device == m.device
+
+    @skip_if_gpu_is_not_available()
+    def test_device_gpu(self, gpu_device: torch.device):
+        m = ModelWrapper(ModelMultiplyP(), gpu_device, True)
+
+        assert m.device.type == "cpu"
+        m.to_default_device()
+        assert m.device == gpu_device
+
+    def test_create_inference(self):
+        m = ModelMultiplyP()
+        mw = ModelWrapper(m, "cpu", True)
+        inference_wrapper = mw.create_inference()
+        assert isinstance(inference_wrapper, InferenceWrapper)
+        assert inference_wrapper.model is not m
+        assert inference_wrapper.model.p is not m.p
+        assert inference_wrapper.model.p == m.p
+
+        mw = ModelWrapper(m, "cpu", False)
+        with pytest.raises(RuntimeError):
+            mw.create_inference()
+
+    def test_infer_cpu(self):
+        mw = ModelWrapper(ModelMultiplyP(), "cpu", True)
+        mw.to_default_device()
+        inference = mw.create_inference()
+
+        data = torch.randn(10)
+        assert isinstance(inference(data), torch.Tensor)
+
+    @skip_if_gpu_is_not_available()
+    def test_infer_gpu(self, gpu_device: torch.device):
+        mw = ModelWrapper(ModelMultiplyP(), gpu_device, True)
+        mw.to_default_device()
+        inference = mw.create_inference()
+
+        data = torch.randn(10)
+        out: torch.Tensor = inference(data)
+        assert out.device == gpu_device
