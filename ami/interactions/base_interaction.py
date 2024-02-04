80f43983a8f41fbed204dba1c6e5594f0afea819
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2024-02-04 10:31:50 +0900

    ADD BaseInteraction class

---
+++ ami/interactions/base_interaction.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class BaseInteraction(ABC):
+    """The base class for all interaction classes."""
+
+    def setup(self) -> None:
+        """Called at the start of the interaction."""
+        pass
+
+    @abstractmethod
+    def step(self) -> None:
+        """Performs a step of interaction.
+
+        This method is called repeatedly by the inference thread.
+        """
+        pass
+
+    def teardown(self) -> None:
+        """Called at the end of the interaction."""
+        pass
