c2178f5f5028c4df245676ecb460a753d3daea60
Yuto Tokunaga <yuntan@untan.xyz>
2024-02-04 14:01:00 +0900

    `make run` で修正

--- ami/logger.py
+++ ami/logger.py
@@ -1,23 +1,29 @@
-import yaml
 import logging
 import logging.config
+
+import yaml
+
 
 def get_main_thread_logger(name: str) -> logging.Logger:
     return __get_thread_logger("main", name)
 
+
 def get_trainer_thread_logger(name: str) -> logging.Logger:
     return __get_thread_logger("trainer", name)
 
+
 def get_inference_thread_logger(name: str) -> logging.Logger:
     return __get_thread_logger("inference", name)
+
 
 def __get_thread_logger(thread_name: str, module_name: str) -> logging.Logger:
     if module_name == "__main__":
         return logging.getLogger(thread_name)
     return logging.getLogger(f"{thread_name}.{module_name}")
 
+
 # Initialization
-with open("sample/logging.yaml", "r") as file:
+with open("sample/logging.yaml") as file:
     config = yaml.safe_load(file)
 
 logging.config.dictConfig(config)
