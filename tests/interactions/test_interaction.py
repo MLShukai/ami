e01bcf273358ad50b25db853ca82edba39de4330
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2024-02-05 15:36:00 +0900

    Move to conftest

--- tests/interactions/test_interaction.py
+++ tests/interactions/test_interaction.py
@@ -8,20 +8,6 @@ from ami.interactions.interaction import BaseAgent, BaseEnvironment, Interaction
 
 class TestInteraction:
     @pytest.fixture
-    def mock_env(self, mocker: MockerFixture) -> Mock:
-        env = mocker.Mock(spec=BaseEnvironment)
-        env.observe.return_value = "observation"
-        return env
-
-    @pytest.fixture
-    def mock_agent(self, mocker: MockerFixture) -> Mock:
-        agent = mocker.Mock(spec=BaseAgent)
-        agent.step.return_value = "action"
-        agent.setup.return_value = "setup_action"
-        agent.teardown.return_value = "teardown_action"
-        return agent
-
-    @pytest.fixture
     def interaction(self, mock_env, mock_agent) -> Interaction:
         return Interaction(mock_env, mock_agent)
 
