135161e50d73b3a444d88e699053bb39a35104d3
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2024-02-06 17:23:52 +0900

    inferenceやtrainingのthread_loggerのテストを追加

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,6 +1,10 @@
 import pytest
 
-from ami.logger import get_main_thread_logger
+from ami.logger import (
+    get_inference_thread_logger,
+    get_main_thread_logger,
+    get_training_thread_logger,
+)
 
 
 @pytest.mark.parametrize("name", ["", ".test", "test.", "try..test"])
@@ -8,3 +12,9 @@ def test_invalid_logger_name(name: str) -> None:
 
     with pytest.raises(ValueError):
         get_main_thread_logger(name)
+
+    with pytest.raises(ValueError):
+        get_training_thread_logger(name)
+
+    with pytest.raises(ValueError):
+        get_inference_thread_logger(name)
