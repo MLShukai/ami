fb91a07a04cc245e4eba68f73f070a744977b99c
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2024-02-11 15:36:05 +0900

    Update docstring and add test for background_thread

--- ami/threads/background_thread.py
+++ ami/threads/background_thread.py
@@ -7,32 +7,39 @@ from .thread_types import ThreadTypes
 
 
 class BackgroundThread(BaseThread):
-    _thread_command_handler: ThreadCommandHandler | None = None
+    """Base class for all background thread objects.
 
-    def __init_subclass__(cls) -> None:
-        super().__init_subclass__()
-        if cls.THREAD_TYPE is ThreadTypes.MAIN:
-            raise RuntimeError("Background `THREAD_TYPE` must not be MAIN!")
+    The `THREAD_TYPE` attribute must not be set to `ThreadType.MAIN`.
+    """
+
+    _thread_command_handler: ThreadCommandHandler | None = None
 
     def __init__(self) -> None:
         super().__init__()
         self._thread = threading.Thread(target=self.run)
 
+        if self.THREAD_TYPE is ThreadTypes.MAIN:
+            raise ValueError("Background `THREAD_TYPE` must not be MAIN!")
+
     @property
     def thread_command_handler(self) -> ThreadCommandHandler:
+        """Retrieves the thread command handler object shared by the main
+        thread."""
         if self._thread_command_handler is None:
             handler = self.get_shared_object(ThreadTypes.MAIN, SharedObjectNames.THREAD_COMMAND_HANDLERS)[
                 self.THREAD_TYPE
             ]
-            if isinstance(handler, ThreadCommandHandler):
-                self._thread_command_handler = handler
-            else:
+            if not isinstance(handler, ThreadCommandHandler):
                 raise RuntimeError(f"Shared thread command handler object is invalid: {handler}")
+            self._thread_command_handler = handler
         return self._thread_command_handler
 
     def start(self) -> None:
         self.logger.info("Starts background thread.")
         self._thread.start()
+
+    def is_alive(self) -> bool:
+        return self._thread.is_alive()
 
     def join(self) -> None:
         self._thread.join()
