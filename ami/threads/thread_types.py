234f7c0d52c7c422474fbfa06fa7149f30e77358
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2024-02-11 13:40:21 +0900

    Update thread types docstring

--- ami/threads/thread_types.py
+++ ami/threads/thread_types.py
@@ -1,9 +1,11 @@
-"""もし設計の拡張で静的なクラス数が増えた場合はここに記述する必要がある．"""
 from enum import Enum, auto
 
 
 class ThreadTypes(Enum):
-    """Enumerates all types of threads that are implemented."""
+    """Enumerates all types of threads that are implemented.
+
+    NOTE: You need add new entry if static thread types are increased.
+    """
 
     MAIN = auto()
     INFERENCE = auto()
