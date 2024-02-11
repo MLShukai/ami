2b53d9b8e1d0f0eefc87e3d352fdfa70ba02163e
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2024-02-11 16:05:31 +0900

    Update import statements

--- ami/threads/__init__.py
+++ ami/threads/__init__.py
@@ -1,3 +1,8 @@
 """This file contains import statements for convenience."""
-from . import base_threads
-from .base_threads import ThreadTypes
+from .base_thread import BaseThread
+from .inference_thread import InferenceThread
+from .main_thread import MainThread
+from .shared_object_pool import SharedObjectNames, SharedObjectPool
+from .thread_control import ThreadCommandHandler, ThreadController
+from .thread_types import ThreadTypes
+from .training_thread import TrainingnThread
