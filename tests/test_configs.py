736ee47beda6b1e21a8b2761bab195d6729cadb1
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-12 22:00:47 +0900

    test_configs.pyでインスタンス化テストを追加

---
+++ tests/test_configs.py
@@ -0,0 +1,30 @@
+"""ここではconfigファイル上からオブジェクトを正常にインスタンス化可能かテストします。"""
+import hydra
+import pytest
+from hydra.utils import instantiate
+
+from tests.helpers import PROJECT_ROOT
+
+CONFIG_DIR = PROJECT_ROOT / "configs"
+LAUNCH_CONFIG = "launch.yaml"
+EXPERIMENT_CONFIG_DIR = CONFIG_DIR / "experiment"
+EXPERIMENT_CONFIG_FILES = EXPERIMENT_CONFIG_DIR.glob("*.*")
+
+EXPERIMENT_CONFIG_OVERRIDES = [[f"experiment={file.name.rsplit('.', 1)[0]}"] for file in EXPERIMENT_CONFIG_FILES]
+HYDRA_OVERRIDES = [None] + EXPERIMENT_CONFIG_OVERRIDES
+
+
+@pytest.mark.parametrize("overrides", HYDRA_OVERRIDES)
+def test_instantiate(overrides: list[str] | None):
+    with hydra.initialize_config_dir(str(CONFIG_DIR)):
+        cfg = hydra.compose(LAUNCH_CONFIG, overrides=overrides)
+
+        interaction = instantiate(cfg.interaction)
+        data_collectors = instantiate(cfg.data_collectors)
+        models = instantiate(cfg.models)
+        trainers = instantiate(cfg.trainers)
+
+        threads = cfg.threads
+        instantiate(threads.main_thread)
+        instantiate(threads.inference_thread, interaction=interaction, data_collectors=data_collectors)
+        instantiate(threads.training_thread, models=models, trainers=trainers)
