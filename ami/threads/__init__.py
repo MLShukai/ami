58138092278740052dfcd2453dc8c5f13559c22f
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-12 20:57:51 +0900

    launchスクリプトで用いるクラスを `threads`名前空間にimport

--- ami/threads/__init__.py
+++ ami/threads/__init__.py
@@ -1,2 +1,6 @@
 """This file contains import statements for convenience."""
+from .base_thread import attach_shared_objects_pool_to_threads
+from .inference_thread import InferenceThread
+from .main_thread import MainThread
 from .thread_types import ThreadTypes
+from .training_thread import TrainingThread
