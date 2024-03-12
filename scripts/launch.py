17056452d49a38d2b61a94a94f3b87dd715d8a46
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-12 20:58:27 +0900

    launch スクリプトを記述

--- scripts/launch.py
+++ scripts/launch.py
@@ -3,8 +3,18 @@ import hydra
 import rootutils
 from omegaconf import DictConfig
 
+from ami.data.utils import DataCollectorsDict
+from ami.interactions.interaction import Interaction
 from ami.logger import get_main_thread_logger
+from ami.models.utils import ModelWrappersDict
 from ami.omegaconf_resolvers import register_custom_resolvers
+from ami.threads import (
+    InferenceThread,
+    MainThread,
+    TrainingThread,
+    attach_shared_objects_pool_to_threads,
+)
+from ami.trainers.utils import TrainersList
 
 # Add the project root path to environment vartiable `PROJECT_ROOT`
 # to refer in the config file by `${oc.env:PROJECT_ROOT}`.
@@ -19,6 +29,49 @@ register_custom_resolvers()
 @hydra.main(config_path="../configs", config_name="launch", version_base="1.3")
 def main(cfg: DictConfig) -> None:
     logger.info("Launch AMI.")
+
+    logger.info(f"Instantiating Interaction <{cfg.interaction._target_}>")
+    interaction: Interaction = hydra.utils.instantiate(cfg.interaction)
+
+    logger.info(f"Instantiating DataCollectors <{cfg.data_collectors._target_}>")
+    data_collectors: DataCollectorsDict = hydra.utils.instantiate(cfg.data_collectors)
+
+    logger.info(f"Instantiating Models <{cfg.models._target_}>")
+    models: ModelWrappersDict = hydra.utils.instantiate(cfg.models)
+    models.send_to_default_device()
+
+    logger.info(f"Instantiating Trainers <{cfg.trainers._target_}>")
+    trainers: TrainersList = hydra.utils.instantiate(cfg.trainers)
+
+    logger.info("Instantiating Thread Classes...")
+    threads_cfg = cfg.threads
+    logger.info(f"Instantiating MainThread: <{threads_cfg.main_thread._target_}>")
+    main_thread: MainThread = hydra.utils.instantiate(threads_cfg.main_thread)
+
+    logger.info(f"Instantiating InferenceThread: <{threads_cfg.inference_thread._target_}>")
+    inference_thread: InferenceThread = hydra.utils.instantiate(
+        threads_cfg.inference_thread, interaction=interaction, data_collectors=data_collectors
+    )
+
+    logger.info(f"Instantiating TrainingThread: <{threads_cfg.training_thread._target_}>")
+    training_thread: TrainingThread = hydra.utils.instantiate(
+        threads_cfg.training_thread, trainers=trainers, models=models
+    )
+
+    logger.info("Sharing objects...")
+    attach_shared_objects_pool_to_threads(main_thread, inference_thread, training_thread)
+
+    logger.info("Starting threads.")
+
+    inference_thread.start()
+    training_thread.start()
+
+    main_thread.run()  # Shutdown 命令または Ctrl+Cが送信されるまでブロッキング
+
+    inference_thread.join()
+    training_thread.join()
+
+    logger.info("Terminated AMI.")
 
 
 if __name__ == "__main__":
