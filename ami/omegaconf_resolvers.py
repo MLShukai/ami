b6ff2d6ff57f6deb61e8584373f8c776bf58a984
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-13 11:11:00 +0900

    一度のみ omegaconf resolverを追加するように修正

--- ami/omegaconf_resolvers.py
+++ ami/omegaconf_resolvers.py
@@ -5,6 +5,12 @@ See: https://omegaconf.readthedocs.io/en/latest/custom_resolvers.html
 import torch
 from omegaconf import OmegaConf
 
+_registered = False
+
 
 def register_custom_resolvers() -> None:
-    OmegaConf.register_new_resolver("torch.device", torch.device)  # Usage: ${torch.device: cuda:0}
+    global _registered
+
+    if not _registered:
+        OmegaConf.register_new_resolver("torch.device", torch.device)  # Usage: ${torch.device: cuda:0}
+        _registered = True
