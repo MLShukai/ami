b1beb6a63f14a86e44e9a5c610ef1c4c35c83e96
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-18 10:40:01 +0900

    configファイル内でpythonの簡易呼び出しを可能に

--- tests/test_omegaconf_resolvers.py
+++ tests/test_omegaconf_resolvers.py
@@ -8,3 +8,4 @@ def test_resolovers():
     register_custom_resolvers()
 
     assert OmegaConf.create({"device": "${torch.device: cuda:0}"}).device == torch.device("cuda:0")
+    assert OmegaConf.create({"eval": "${python.eval: 1 + 2 * 3 / 4}"}).eval == 2.5
