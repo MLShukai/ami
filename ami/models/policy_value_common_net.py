c0d299f72f5ac7548876b69fb747338212f3f779
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-23 22:59:30 +0900

    ModularPolicyValueCommonNetを追加

--- ami/models/policy_value_common_net.py
+++ ami/models/policy_value_common_net.py
@@ -3,6 +3,7 @@ from typing import Any
 
 import torch
 import torch.nn as nn
+from torch import Tensor
 from torch.distributions import Distribution
 
 
@@ -13,3 +14,15 @@ class PolicyValueCommonNet(ABC, nn.Module):
     def forward(self, *args: Any, **kwds: Any) -> tuple[Distribution, torch.Tensor]:
         """Returns the action distribution and estimated value."""
         raise NotImplementedError
+
+
+class ModularPolicyValueCommonNet(PolicyValueCommonNet):
+    def __init__(self, base_model: nn.Module, policy_head: nn.Module, value_head: nn.Module) -> None:
+        super().__init__()
+        self.base_model = base_model
+        self.policy_head = policy_head
+        self.value_head = value_head
+
+    def forward(self, *args: Any, **kwds: Any) -> tuple[Distribution, Tensor]:
+        h = self.base_model(*args, **kwds)
+        return self.policy_head(h), self.value_head(h)
