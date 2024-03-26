1618560354aaa6bb6092ec4efab0d02baea044a5
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-03-26 12:42:02 +0900

    Removed `_COMMON`

--- ami/models/model_names.py
+++ ami/models/model_names.py
@@ -4,4 +4,4 @@ from enum import StrEnum
 class ModelNames(StrEnum):
     IMAGE_ENCODER = "image_encoder"
     IMAGE_DECODER = "image_decoder"
-    POLICY_VALUE_COMMON = "policy_value_common"
+    POLICY_VALUE = "policy_value"
