5ba4789f07552690059bdd0ba89793856c743071
Yuto Tokunaga <git@yuntan.tech>
2024-03-30 17:26:20 +0900

    テストを修正

--- tests/interactions/environments/actuators/test_vrchat_osc_discrete_actuator.py
+++ tests/interactions/environments/actuators/test_vrchat_osc_discrete_actuator.py
@@ -1,7 +1,9 @@
 import pytest
 import torch
+from vrchat_io.controller.osc import Buttons
 
 from ami.interactions.environments.actuators.vrchat_osc_discrete_actuator import (
+    ACTION_CHOICES_PER_CATEGORY,
     VRChatOSCDiscreteActuator,
 )
 
@@ -13,5 +15,57 @@ class TestVRChatOSCDiscreteActuator:
 
     def test_operate(self, actuator: VRChatOSCDiscreteActuator) -> None:
         actuator.setup()
+
         actuator.operate(torch.tensor([1, 1, 1, 1, 1], dtype=torch.long))
+
+        with pytest.raises(ValueError):
+            actuator.operate(torch.tensor(ACTION_CHOICES_PER_CATEGORY))
+
         actuator.teardown()
+
+    @pytest.mark.parametrize(
+        "action, expected_command",
+        [
+            (
+                [0, 0, 0, 0, 0],
+                {
+                    Buttons.MoveForward: 0,
+                    Buttons.MoveBackward: 0,
+                    Buttons.MoveLeft: 0,
+                    Buttons.MoveRight: 0,
+                    Buttons.LookLeft: 0,
+                    Buttons.LookRight: 0,
+                    Buttons.Jump: 0,
+                    Buttons.Run: 0,
+                },
+            ),
+            (
+                [1, 1, 1, 1, 1],
+                {
+                    Buttons.MoveForward: 1,
+                    Buttons.MoveBackward: 0,
+                    Buttons.MoveLeft: 1,
+                    Buttons.MoveRight: 0,
+                    Buttons.LookLeft: 1,
+                    Buttons.LookRight: 0,
+                    Buttons.Jump: 1,
+                    Buttons.Run: 1,
+                },
+            ),
+            (
+                [2, 2, 2, 0, 0],
+                {
+                    Buttons.MoveForward: 0,
+                    Buttons.MoveBackward: 1,
+                    Buttons.MoveLeft: 0,
+                    Buttons.MoveRight: 1,
+                    Buttons.LookLeft: 0,
+                    Buttons.LookRight: 1,
+                    Buttons.Jump: 0,
+                    Buttons.Run: 0,
+                },
+            ),
+        ],
+    )
+    def test_convert_action_to_command(self, actuator, action, expected_command):
+        assert actuator.convert_action_to_command(action) == expected_command
