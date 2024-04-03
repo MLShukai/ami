489c0703888385a743f8ad6de018535aca3372f9
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-04-04 07:44:59 +0900

    stepとsetupの共通部分を`_common_step`メソッドにした

--- ami/interactions/agents/curiosity_image_ppo_agent.py
+++ ami/interactions/agents/curiosity_image_ppo_agent.py
@@ -45,61 +45,51 @@ class CuriosityImagePPOAgent(BaseAgent[Tensor, Tensor]):
     forward_dynamics_hidden_state: Tensor
     step_data: StepData
 
-    def setup(self, observation: Tensor) -> Tensor:
-        super().setup(observation)
+    def _common_step(self, observation: Tensor, initial_step: bool = False) -> Tensor:
+        """Common step procedure for agent.
 
-        self.step_data = StepData()
-
-        embed_obs: Tensor = self.image_encoder(observation)
-        self.step_data[DataKeys.OBSERVATION] = observation
-        self.step_data[DataKeys.EMBED_OBSERVATION] = embed_obs
-
-        self.image_collector.collect(self.step_data)
-
-        action_dist, value = self.policy_value(observation)
-        action = action_dist.sample()
-        self.step_data[DataKeys.ACTION] = action
-        self.step_data[DataKeys.ACTION_LOG_PROBABILITY] = action_dist.log_prob(action)
-        self.step_data[DataKeys.VALUE] = value
-
-        self.step_data[DataKeys.HIDDEN] = self.forward_dynamics_hidden_state
-
-        self.forward_dynamics_trajectory_collector.collect(self.step_data)
-        pred, hidden = self.forward_dynamics(embed_obs, self.forward_dynamics_hidden_state, action)
-        self.predicted_next_embed_observation_dist = pred
-        self.forward_dynamics_hidden_state = hidden
-
-        return action
-
-    def step(self, observation: Tensor) -> Tensor:
+        If `initial_step` is False, some procedures are skipped.
+        """
 
         # \phi(o_t) -> z_t
         embed_obs = self.image_encoder(observation)
-        reward = -self.predicted_next_embed_observation_dist.log_prob(embed_obs)
-        self.step_data[DataKeys.REWARD] = reward
 
-        self.ppo_trajectory_collector.collect(self.step_data)  # o_t, a_t, log p(a_t), v_t, r_{t+1}
+        if not initial_step:
+            # 報酬計算は初期ステップではできないためスキップ。
+            reward = -self.predicted_next_embed_observation_dist.log_prob(embed_obs)
+            self.step_data[DataKeys.REWARD] = reward  # r_{t+1}
+
+            # ステップの冒頭でデータコレクトすることで前ステップのデータを収集する。
+            self.ppo_trajectory_collector.collect(self.step_data)  # o_t, a_t, log p(a_t), v_t, r_{t+1}
+            self.image_collector.collect(self.step_data)
+            self.forward_dynamics_trajectory_collector.collect(self.step_data)
 
         action_dist, value = self.policy_value(observation)
 
-        self.step_data[DataKeys.OBSERVATION] = observation
-        self.step_data[DataKeys.EMBED_OBSERVATION] = embed_obs
-
-        self.image_collector.collect(self.step_data)
+        self.step_data[DataKeys.OBSERVATION] = observation  # o_t
+        self.step_data[DataKeys.EMBED_OBSERVATION] = embed_obs  # z_t
 
         action_dist, value = self.policy_value(observation)
         action = action_dist.sample()
         action_log_prob = action_dist.log_prob(action)
 
-        self.step_data[DataKeys.ACTION] = action
-        self.step_data[DataKeys.ACTION_LOG_PROBABILITY] = action_log_prob
-        self.step_data[DataKeys.VALUE] = value
-        self.step_data[DataKeys.HIDDEN] = self.forward_dynamics_hidden_state
-
-        self.forward_dynamics_trajectory_collector.collect(self.step_data)
+        self.step_data[DataKeys.ACTION] = action  # a_t
+        self.step_data[DataKeys.ACTION_LOG_PROBABILITY] = action_log_prob  # log \pi(a_t | o_t)
+        self.step_data[DataKeys.VALUE] = value  # v_t
+        self.step_data[DataKeys.HIDDEN] = self.forward_dynamics_hidden_state  # h_t
 
         pred, hidden = self.forward_dynamics(embed_obs, self.forward_dynamics_hidden_state, action)
-        self.predicted_next_embed_observation_dist = pred
-        self.forward_dynamics_hidden_state = hidden
+        self.predicted_next_embed_observation_dist = pred  # p(\hat{z}_{t+1} | z_t, h_t, a_t)
+        self.forward_dynamics_hidden_state = hidden  # h_{t+1}
 
         return action
+
+    def setup(self, observation: Tensor) -> Tensor:
+        super().setup(observation)
+
+        self.step_data = StepData()
+
+        return self._common_step(observation, initial_step=True)
+
+    def step(self, observation: Tensor) -> Tensor:
+        return self._common_step(observation, initial_step=False)
