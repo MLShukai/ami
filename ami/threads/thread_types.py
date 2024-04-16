b5601273b7531318c94b1da4d83a09ebaaa4c699
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-04-16 20:51:19 +0900

    `get_thread_name_from_type`を`thread_type.py`に移動

--- ami/threads/thread_types.py
+++ ami/threads/thread_types.py
@@ -13,3 +13,29 @@ class ThreadTypes(Enum):
 
 
 BACKGROUND_THREAD_TYPES = {e for e in ThreadTypes if e is not ThreadTypes.MAIN}
+
+
+def get_thread_name_from_type(thread_type: ThreadTypes) -> str:
+    """Returns the thread name corresponding to the given `thread_type`.
+
+    Args:
+        thread_type: The type of thread as defined in ThreadTypes enum.
+
+    Raises:
+        NotImplementedError: If the name for the specified thread type is not implemented yet.
+        ValueError: If an invalid thread type object is provided.
+
+    Returns:
+        str: The name of the thread corresponding to the given thread type.
+    """
+    match thread_type:
+        case ThreadTypes.MAIN:
+            return "main"
+        case ThreadTypes.INFERENCE:
+            return "inference"
+        case ThreadTypes.TRAINING:
+            return "training"
+        case _:
+            if thread_type in ThreadTypes:
+                raise NotImplementedError(f"The name for specified thread type '{thread_type}' is not implemented yet.")
+            raise ValueError(f"Invalid thread type object: {thread_type}")
