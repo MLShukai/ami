4858e769b4dc76304fe0d86f1c084f33f99305e8
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-04-30 11:56:07 +0900

    ThreadSafeFlagを追加

---
+++ tests/threads/test_utils.py
@@ -0,0 +1,27 @@
+import pytest
+
+from ami.threads.utils import ThreadSafeFlag
+
+
+class TestThreadSafeFlag:
+    @pytest.fixture
+    def flag(self) -> ThreadSafeFlag:
+        return ThreadSafeFlag()
+
+    def test_is_set_when_inital(self, flag):
+        assert flag.is_set() is False, "Flag should be initially False"
+
+    def test_set(self, flag):
+        flag.set()
+        assert flag.is_set() is True, "Flag should be set to True"
+
+    def test_clear(self, flag):
+        flag.set()
+        flag.clear()
+        assert flag.is_set() is False, "Flag should be cleared to False"
+
+    def test_get(self, flag):
+        assert flag.get() is False
+        flag.set()
+        assert flag.get() is True, "Get should return True"
+        assert flag.is_set() is False, "Flag should be cleared after get"
