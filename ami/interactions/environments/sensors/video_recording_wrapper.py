195bd36314f3d63cbd0af4dc5609b962993fe7bf
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-05-11 20:56:37 +0900

    VideoRecordingWrapperを追加

---
+++ ami/interactions/environments/sensors/video_recording_wrapper.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from pathlib import Path
+
+import cv2
+import numpy as np
+from torch import Tensor
+
+from ami.interactions.environments.sensors.base_sensor import BaseSensor
+
+from .base_sensor import BaseSensorWrapper
+
+
+class VideoRecordingWrapper(BaseSensorWrapper[Tensor, Tensor]):
+    """Records the image sensor data to file."""
+
+    def __init__(
+        self,
+        sensor: BaseSensor[Tensor],
+        output_dir: str,
+        width: int,
+        height: int,
+        frame_rate: float,
+        file_name_format: str = "%Y-%m-%d_%H-%M-%S.%f.mp4",
+        fourcc: str = "mp4v",
+        do_rgb_to_bgr: bool = True,
+        do_scale_255: bool = True,
+    ) -> None:
+        """
+        Constructs the sensor wrapper.
+
+        Args:
+            sensor: An instance of the image sensor.
+            output_dir: Path to the directory where the video will be saved.
+            width: Width of the image to be recorded.
+            height: Height of the image to be recorded.
+            frame_rate: Frame rate at which the video is recorded.
+            fourcc: FourCC (Four Character Code) used for the video writer.
+            file_name_format: File name format that includes a datetime format string.
+            do_rgb_to_bgr: Whether to convert RGB format tensors to BGR format.
+            do_scale_255: Whether to scale values by 255.
+        """
+
+        super().__init__(sensor)
+
+        self.output_dir = Path(output_dir)
+        self.output_dir.mkdir(exist_ok=True)
+        self.frame_size = (width, height)
+        self.frame_rate = frame_rate
+        self.file_name_format = file_name_format
+        self.fourcc = fourcc
+        self.do_rgb_to_bgr = do_rgb_to_bgr
+        self.do_scale_255 = do_scale_255
+
+    def setup_video_writer(self) -> None:
+        codec = cv2.VideoWriter_fourcc(*self.fourcc)  # type: ignore
+        self.video_path = self.output_dir / datetime.now().strftime(self.file_name_format)
+        self.video_writer = cv2.VideoWriter(str(self.video_path), codec, self.frame_rate, self.frame_size)
+
+    def setup(self) -> None:
+        super().setup()
+        self.setup_video_writer()
+
+    def wrap_observation(self, observation: Tensor) -> Tensor:
+        """Write image observation to file.
+
+        Observation shape is (C, H, W).
+        """
+        frame = observation.detach().cpu().clone().numpy()
+
+        frame = np.moveaxis(frame, 0, -1)
+        if self.do_scale_255:
+            frame = frame * 255
+
+        frame = frame.astype(np.uint8)
+
+        if self.do_rgb_to_bgr:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+        self.video_writer.write(frame)
+
+        return observation
+
+    def teardown(self) -> None:
+        super().teardown()
+        self.video_writer.release()
+
+    def on_paused(self) -> None:
+        super().on_paused()
+        self.video_writer.release()  # 経験が途切れるため一度リリース
+
+    def on_resumed(self) -> None:
+        super().on_resumed()
+        self.setup_video_writer()
---
+++ tests/interactions/environments/sensors/test_video_recording_wrapper.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+import pytest
+import torch
+from pytest_mock import MockerFixture
+
+from ami.interactions.environments.sensors.video_recording_wrapper import (
+    BaseSensor,
+    VideoRecordingWrapper,
+)
+
+WIDTH = 480
+HEIGHT = 640
+CHANNELS = 3
+
+
+class TestVideoRecordingWrapper:
+
+    READ_IMAGE = torch.ones(CHANNELS, HEIGHT, WIDTH)
+
+    @pytest.fixture
+    def mock_image_sensor(self, mocker: MockerFixture):
+        mock = mocker.Mock(BaseSensor)
+        mock.read.return_value = self.READ_IMAGE.clone()
+        return mock
+
+    @pytest.fixture
+    def wrapper(self, mock_image_sensor, tmp_path):
+        sensor = VideoRecordingWrapper(mock_image_sensor, tmp_path / "video", WIDTH, HEIGHT, 30.0)
+        sensor.setup()
+        yield sensor
+        sensor.teardown()
+
+    def test_setup_video_writer(self, wrapper: VideoRecordingWrapper):
+        assert wrapper.video_path.exists()
+
+    def test_wrap_observation(self, wrapper: VideoRecordingWrapper):
+
+        obs = wrapper.read()
+        assert torch.equal(obs, self.READ_IMAGE)
+
+    def test_recorded_video(self, wrapper: VideoRecordingWrapper):
+        for _ in range(30):
+            wrapper.read()
+        wrapper.teardown()
+
+        cap = cv2.VideoCapture(str(wrapper.video_path))
+
+        for _ in range(30):
+            ret, frame = cap.read()
+            assert ret
+            np.testing.assert_almost_equal(np.full((HEIGHT, WIDTH, CHANNELS), 255, dtype=np.uint8), frame, -1)
+
+    def test_on_paused_and_resumed(self, wrapper: VideoRecordingWrapper):
+        wrapper.read()
+        wrapper.on_paused()
+        previus_file = wrapper.video_path
+        wrapper.on_resumed()
+        assert previus_file != wrapper.video_path
