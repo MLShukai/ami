839e10608ffcbf2806dff3666d627cd2e07cd49f
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-05-11 10:45:48 +0900

    未使用のDataKeyをコメントアウト

--- ami/data/step_data.py
+++ ami/data/step_data.py
@@ -8,17 +8,17 @@ class DataKeys(StrEnum):
     """Enumerates all the names of data obtained from the interaction between
     the environment and the agent."""
 
-    PREVIOUS_ACTION = "previous_action"  # a_{t-1}
+    # PREVIOUS_ACTION = "previous_action"  # a_{t-1}
     OBSERVATION = "observation"  # o_t
     EMBED_OBSERVATION = "embed_observation"  # z_t
     ACTION = "action"  # a_t
     ACTION_LOG_PROBABILITY = "action_log_probability"
     VALUE = "value"  # v_t
-    PREDICTED_NEXT_EMBED_OBSERVATION = "predicted_next_embed_observation"  # \hat{z}_{t+1}
-    NEXT_OBSERVATION = "next_observation"  # o_{t+1}
-    NEXT_EMBED_OBSERVATION = "next_embed_observation"  # z_{t+1}
+    # PREDICTED_NEXT_EMBED_OBSERVATION = "predicted_next_embed_observation"  # \hat{z}_{t+1}
+    # NEXT_OBSERVATION = "next_observation"  # o_{t+1}
+    # NEXT_EMBED_OBSERVATION = "next_embed_observation"  # z_{t+1}
     REWARD = "reward"  # r_{t+1}
-    NEXT_VALUE = "next_value"  # v_{t+1}
+    # NEXT_VALUE = "next_value"  # v_{t+1}
     HIDDEN = "hidden"  # h_t
 
 
