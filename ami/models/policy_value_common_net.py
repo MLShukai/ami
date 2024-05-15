e77eb9a8150aa7d310107dca91d51afcc4ac257c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-05-15 16:55:16 +0900

    PolicyValueCommonNetのbase_modelをcore_modelにリネーム

--- ami/models/policy_value_common_net.py
+++ ami/models/policy_value_common_net.py
@@ -6,13 +6,13 @@ from torch.distributions import Distribution
 class PolicyValueCommonNet(nn.Module):
     """Modules with shared models for policy and value functions."""
 
-    def __init__(self, base_model: nn.Module, policy_head: nn.Module, value_head: nn.Module) -> None:
+    def __init__(self, core_model: nn.Module, policy_head: nn.Module, value_head: nn.Module) -> None:
         super().__init__()
-        self.base_model = base_model
+        self.core_model = core_model
         self.policy_head = policy_head
         self.value_head = value_head
 
     def forward(self, observation: Tensor) -> tuple[Distribution, Tensor]:
         """Returns the action distribution and estimated value."""
-        h = self.base_model(observation)
+        h = self.core_model(observation)
         return self.policy_head(h), self.value_head(h)
