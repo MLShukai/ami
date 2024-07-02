f71a28f2bb8d354dfa29c267433ff49e16840ec2
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-07-02 09:15:25 +0900

    apt のinstallに-yを追加

--- scripts/setup-computer-ubuntu22.04.sh
+++ scripts/setup-computer-ubuntu22.04.sh
@@ -22,7 +22,7 @@ sudo apt install -y curl openssh-server git tmux nvidia-driver-550
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -34,7 +34,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run --rm hello-world
 
