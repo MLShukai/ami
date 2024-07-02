b033ddf20c21db8ab0ed47582248921154230183
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-07-02 09:12:27 +0900

    curl を追加

--- scripts/setup-computer-ubuntu22.04.sh
+++ scripts/setup-computer-ubuntu22.04.sh
@@ -16,7 +16,7 @@ sudo perl -p -i.bak -e 's%(deb(?:-src|)\s+)https?://(?!archive\.canonical\.com|s
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y openssh-server git tmux nvidia-driver-550
+sudo apt install -y curl openssh-server git tmux nvidia-driver-550
 
 # --- Docker https://docs.docker.com/engine/install/ubuntu/ ---
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
