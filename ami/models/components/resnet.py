b84ce344eb4ab36eaf1a70ee6080a0a975b145ba
myxyy <m.myxomatosis@gmail.com>
2024-07-22 22:41:15 +0900

    core_modelにresnetを入れる

---
+++ ami/models/components/resnet.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+from torch import Tensor
+
+
+class ResNetFF(nn.Module):
+    def __init__(self, dim: int, dim_hidden: int, depth: int, activation: nn.Module = nn.ReLU()):
+        super().__init__()
+        self.ff_list = nn.ModuleList(
+            [nn.Sequential(nn.Linear(dim, dim_hidden), activation, nn.Linear(dim_hidden, dim)) for _ in range(depth)]
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        for ff in self.ff_list:
+            x_ = x
+            x = ff(x)
+            x = x + x_
+        return x
