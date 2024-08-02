7288816a6bbe3b18262f2052e84ff4a3b1642689
zassou65535 <gravity@kuf.biglobe.ne.jp>
2024-08-02 16:55:10 +0900

    testを書く

--- tests/models/components/test_vision_transformer_layer.py
+++ tests/models/components/test_vision_transformer_layer.py
@@ -13,14 +13,22 @@ class TestVisionTransformerLayer:
     @pytest.mark.parametrize("mlp_ratio", [4.0, 48/11])
     # test input params
     @pytest.mark.parametrize("batch_size", [1, 4])
+    @pytest.mark.parametrize("max_n_patches", [1, 512])
     def test_vision_transformer_layer(
         self,
         embedding_dim: int,
         num_heads: int,
         mlp_ratio: float,
+        batch_size: int,
+        max_n_patches: int,
     ):
         vision_transformer_layer = VisionTransformerLayer(
             embedding_dim=embedding_dim,
             num_heads=num_heads,
             mlp_ratio=mlp_ratio,
         )
+        # define input
+        n_patches = random.randint(1, max_n_patches) # including max_n_patches
+        input_patches = torch.randn([batch_size, n_patches, embedding_dim], dtype=torch.float)
+        output_patches = vision_transformer_layer(input_patches)
+        assert input_patches.size()==output_patches.size()
