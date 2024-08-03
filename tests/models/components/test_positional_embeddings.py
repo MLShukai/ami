250b49b845c35fe1f860c8a7fdaeb80c2305971a
zassou <51876515+zassou65535@users.noreply.github.com>
2024-08-03 15:02:11 +0900

    Apply suggestions from code review
    Testに関してsuggestionを反映
    
    Co-authored-by: GesonAnko <59220704+Geson-anko@users.noreply.github.com>

--- tests/models/components/test_positional_embeddings.py
+++ tests/models/components/test_positional_embeddings.py
@@ -8,17 +8,12 @@ from ami.models.components.positional_embeddings import get_2d_positional_embedd
 
 class TestPositionalEmbeddings:
     @pytest.mark.parametrize("embed_dim", [384, 768])
-    @pytest.mark.parametrize("max_grid_size", [(1), (128), (1, 1), (1, 128), (128, 1), (128, 128)])
+    @pytest.mark.parametrize("grid_size", [(1), (128), (1, 1), (1, 128), (128, 1), (128, 128)])
     def test_get_2d_positional_embeddings(
         self,
         embed_dim: int,
-        max_grid_size: int | tuple[int, int],
+        grid_size: int | tuple[int, int],
     ):
-        grid_size: int | tuple[int, int] = (
-            random.randint(1, max_grid_size)
-            if isinstance(max_grid_size, int)
-            else (random.randint(1, max_grid_size[0]), random.randint(1, max_grid_size[1]))  # (height, width)
-        )
         positional_embeddings = get_2d_positional_embeddings(embed_dim=embed_dim, grid_size=grid_size)
         assert positional_embeddings.ndim == 2, "positional_embeddings.ndim mismatch."
         expected_n_tokens = (
@@ -26,4 +21,4 @@ class TestPositionalEmbeddings:
         )
         assert positional_embeddings.shape[0] == expected_n_tokens, "num of tokens mismatch."
         assert positional_embeddings.shape[1] == embed_dim, "dim mismatch."
-        assert np.all(np.abs(positional_embeddings) <= 1.0), "some invalid values."
+        assert np.all(np.abs(positional_embeddings) <= 1.0), "some invalid values of sin and cos function"
