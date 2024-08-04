cc2718041fa2dd05aa46a5620d8a7938c148a99e
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-08-04 15:49:52 +0900

    テストのport番号を変更

--- tests/threads/test_web_api_handler.py
+++ tests/threads/test_web_api_handler.py
@@ -86,18 +86,20 @@ class TestWebApiHandler:
         assert "error" in response.json
 
     def test_increment_port_when_already_used(self, controller: ThreadController) -> None:
+        PORT = 8092
+
         controller = ThreadController()
         status = ThreadControllerStatus(controller)
-        handler1 = WebApiHandler(status, "localhost", 8090)
+        handler1 = WebApiHandler(status, "localhost", PORT)
         handler1.run_in_background()
 
-        response = requests.get("http://localhost:8090/api/status")
+        response = requests.get(f"http://localhost:{PORT}/api/status")
         assert response.status_code == 200
 
         with pytest.raises(requests.exceptions.ConnectionError):
-            requests.get("http://localhost:8091/api/status")
+            requests.get(f"http://localhost:{PORT+1}/api/status")
 
-        handler2 = WebApiHandler(status, "localhost", 8090)
+        handler2 = WebApiHandler(status, "localhost", PORT)
         handler2.run_in_background()
-        response = requests.get("http://localhost:8091/api/status")
+        response = requests.get(f"http://localhost:{PORT+1}/api/status")
         assert response.status_code == 200
