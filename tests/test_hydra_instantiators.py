7a5077338f751f7a90cd98f939120044602719bb
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-08-06 17:00:48 +0900

    format by pre-commit

--- tests/test_hydra_instantiators.py
+++ tests/test_hydra_instantiators.py
@@ -1,37 +1,40 @@
+import pytest
+from omegaconf import OmegaConf
+
 from ami.hydra_instantiators import instantiate_models
 from ami.models.model_names import ModelNames
-from omegaconf import OmegaConf
-import pytest
 
 
 def test_instantiate_models():
-    # Test instantiate 
-    cfg = OmegaConf.create("""\
-    image_decoder:
-      _target_: ami.models.model_wrapper.ModelWrapper
-      model:
-        _target_: ami.models.vae.Conv2dDecoder
-        height: 84
-        width: 84
-        channels: 3
-        latent_dim: 512
-    """
+    # Test instantiate
+    cfg = OmegaConf.create(
+        """\
+        image_decoder:
+          _target_: ami.models.model_wrapper.ModelWrapper
+          model:
+            _target_: ami.models.vae.Conv2dDecoder
+            height: 84
+            width: 84
+            channels: 3
+            latent_dim: 512
+        """
     )
     models = instantiate_models(cfg)
     assert ModelNames.IMAGE_DECODER in models
 
     # Test alias
-    cfg = OmegaConf.create("""\
-    image_decoder:
-      _target_: ami.models.model_wrapper.ModelWrapper
-      model:
-        _target_: ami.models.vae.Conv2dDecoder
-        height: 84
-        width: 84
-        channels: 3
-        latent_dim: 512
-    image_encoder: image_decoder
-    """
+    cfg = OmegaConf.create(
+        """\
+        image_decoder:
+          _target_: ami.models.model_wrapper.ModelWrapper
+          model:
+            _target_: ami.models.vae.Conv2dDecoder
+            height: 84
+            width: 84
+            channels: 3
+            latent_dim: 512
+        image_encoder: image_decoder
+        """
     )
     models = instantiate_models(cfg)
     assert ModelNames.IMAGE_DECODER in models
@@ -39,17 +42,18 @@ def test_instantiate_models():
     assert models[ModelNames.IMAGE_ENCODER] is models[ModelNames.IMAGE_DECODER]
 
     # Test invalid config format
-    cfg = OmegaConf.create("""\
-    image_decoder:
-        _target_: ami.models.model_wrapper.ModelWrapper
-        model:
-          _target_: ami.models.vae.Conv2dDecoder
-          height: 84
-          width: 84
-          channels: 3
-          latent_dim: 512
-    image_encoder: 0 # <- not str value is allowed!
-    """
+    cfg = OmegaConf.create(
+        """\
+        image_decoder:
+            _target_: ami.models.model_wrapper.ModelWrapper
+            model:
+              _target_: ami.models.vae.Conv2dDecoder
+              height: 84
+              width: 84
+              channels: 3
+              latent_dim: 512
+        image_encoder: 0 # <- not str value is allowed!
+        """
     )
     with pytest.raises(RuntimeError):
-      instantiate_models(cfg)
+        instantiate_models(cfg)
