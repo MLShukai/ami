a456a851068c4e0a59a457734d47a4e35f6cdbc7
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-08-06 10:54:58 +0900

    ModelWrapperのinferの実装を依存性抽出

--- ami/models/model_wrapper.py
+++ ami/models/model_wrapper.py
@@ -2,12 +2,42 @@ from __future__ import annotations
 
 import copy
 import threading
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, Protocol, TypeVar
 
 import torch
 import torch.nn as nn
 
 ModuleType = TypeVar("ModuleType", bound=nn.Module)
+
+
+class InferenceForwardCallable(Protocol):
+    """Typing for `inference_forward` argument of ModelWrapper because
+    `typing.Callable` can not typing `*args` and `**kwds`."""
+
+    def __call__(self, wrapper: ModelWrapper[Any], *args: Any, **kwds: Any) -> Any:
+        ...
+
+
+def default_infer(wrapper: ModelWrapper[Any], *args: Any, **kwds: Any) -> Any:
+    """Default inference forward flow.
+
+    Tensors in `args` and `kwds` are sent to the computing device. If
+    you override this method, be careful to send the input tensor to the
+    computing device.
+    """
+    device = wrapper.device
+    new_args, new_kwds = [], {}
+    for i in args:
+        if isinstance(i, torch.Tensor):
+            i = i.to(device)
+        new_args.append(i)
+
+    for k, v in kwds.items():
+        if isinstance(v, torch.Tensor):
+            v = v.to(device)
+        new_kwds[k] = v
+
+    return wrapper(*new_args, **new_kwds)
 
 
 class ModelWrapper(nn.Module, Generic[ModuleType]):
@@ -26,6 +56,7 @@ class ModelWrapper(nn.Module, Generic[ModuleType]):
         model: ModuleType,
         default_device: torch.device = torch.device("cpu"),
         has_inference: bool = True,
+        inference_forward: InferenceForwardCallable = default_infer,
     ) -> None:
         """Constructs the model wrapper.
 
@@ -33,12 +64,14 @@ class ModelWrapper(nn.Module, Generic[ModuleType]):
             model: The deep neural network model.
             default_device: The default device for model creation. This is used to determine the `device` property if the model lacks parameters or buffers.
             has_inference: Specifies whether the wrapper contains an inference component.
+            inference_forward: The inference forward flow for the wrapped model.
         """
 
         super().__init__()
         self.model = model
         self._default_device = torch.device(default_device)
         self.has_inference = has_inference
+        self._inference_forward = inference_forward
 
     def forward(self, *args: Any, **kwds: Any) -> Any:
         """Executes the forward path for the training."""
@@ -54,25 +87,8 @@ class ModelWrapper(nn.Module, Generic[ModuleType]):
         return self._default_device
 
     def infer(self, *args: Any, **kwds: Any) -> Any:
-        """Performs the inference.
-
-        Tensors in `args` and `kwds` are sent to the computing device.
-        If you override this method, be careful to send the input tensor
-        to the computing device.
-        """
-        device = self.device
-        new_args, new_kwds = [], {}
-        for i in args:
-            if isinstance(i, torch.Tensor):
-                i = i.to(device)
-            new_args.append(i)
-
-        for k, v in kwds.items():
-            if isinstance(v, torch.Tensor):
-                v = v.to(device)
-            new_kwds[k] = v
-
-        return self.model(*new_args, **new_kwds)
+        """Performs the inference."""
+        return self._inference_forward(self, *args, **kwds)
 
     def to_default_device(self) -> None:
         """Sends the model to the default computing device."""
