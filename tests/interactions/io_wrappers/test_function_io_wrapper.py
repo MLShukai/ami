7df1bf6e1cc9ab5b08f7ec5c4e577684000bf6e2
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-08-10 15:45:37 +0900

    normalize tensorを追加

--- tests/interactions/io_wrappers/test_function_io_wrapper.py
+++ tests/interactions/io_wrappers/test_function_io_wrapper.py
@@ -1,6 +1,11 @@
+import pytest
+import torch
 from pytest_mock import MockerFixture
 
-from ami.interactions.io_wrappers.function_wrapper import FunctionIOWrapper
+from ami.interactions.io_wrappers.function_wrapper import (
+    FunctionIOWrapper,
+    normalize_tensor,
+)
 
 
 class TestFunctionIOWrapper:
@@ -8,3 +13,10 @@ class TestFunctionIOWrapper:
         wrap_function = mocker.Mock(return_value="wrapped")
         wrapper = FunctionIOWrapper(wrap_function)
         assert wrapper.wrap("input") == "wrapped"
+
+
+def test_normalize_tensor():
+    for _ in range(10):
+        out = normalize_tensor(torch.rand(100))
+        assert out.mean() == pytest.approx(0.0, abs=1e-5)
+        assert out.std() == pytest.approx(1.0, abs=1e-5)
