9e802f7c17c9ad1925a48e363c39ca7451084d55
myxyy <m.myxomatosis@gmail.com>
2024-08-10 15:55:58 +0900

    テスト等ある程度修正

--- ami/interactions/environments/unity_environment.py
+++ ami/interactions/environments/unity_environment.py
@@ -1,4 +1,5 @@
 from typing import Any
+from uuid import UUID
 
 import torch
 from mlagents_envs.environment import UnityEnvironment as RawUnityEnv
@@ -6,23 +7,20 @@ from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
 from mlagents_envs.side_channel.engine_configuration_channel import (
     EngineConfigurationChannel,
 )
+from mlagents_envs.side_channel.side_channel import IncomingMessage, SideChannel
 from numpy.typing import NDArray
 from torch import Tensor
 from typing_extensions import override
 
 from .base_environment import BaseEnvironment
-from mlagents_envs.side_channel.side_channel import (
-    SideChannel,
-    IncomingMessage,
-)
-import uuid
+
 
 class TransformLogChannel(SideChannel):
-    def __init__(self, id, log_file_path):
+    def __init__(self, id: UUID, log_file_path: str):
         super().__init__(id)
         self.log_file_path = log_file_path
 
-    def on_message_received(self, msg: IncomingMessage):
+    def on_message_received(self, msg: IncomingMessage) -> None:
         with open(self.log_file_path, mode="a") as f:
             value_list = msg.read_float32_list()
             frame_count = value_list[0]
@@ -32,8 +30,11 @@ class TransformLogChannel(SideChannel):
             euler_x = value_list[4]
             euler_y = value_list[5]
             euler_z = value_list[6]
-            format_string = f"{frame_count}, {position_x}, {position_y}, {position_z}, {euler_x}, {euler_y}, {euler_z}\n"
+            format_string = (
+                f"{frame_count}, {position_x}, {position_y}, {position_z}, {euler_x}, {euler_y}, {euler_z}\n"
+            )
             f.write(format_string)
+
 
 class UnityEnvironment(BaseEnvironment[Tensor, Tensor]):
     """A class for connecting to and interacting with a Unity ML Agents
@@ -48,7 +49,7 @@ class UnityEnvironment(BaseEnvironment[Tensor, Tensor]):
     def __init__(
         self,
         file_path: str,
-        log_file_path: str,
+        log_file_path: str | None = None,
         worker_id: int = 0,
         base_port: int | None = None,
         seed: int = 0,
@@ -73,14 +74,17 @@ class UnityEnvironment(BaseEnvironment[Tensor, Tensor]):
         super().__init__()
 
         self.engine_configuration_channel = EngineConfigurationChannel()
-        transform_log_channel = TransformLogChannel(uuid.UUID("621f0a70-4f87-11ea-a6bf-784f4387d1f7"), log_file_path)
+        if log_file_path is not None:
+            transform_log_channel = TransformLogChannel(UUID("621f0a70-4f87-11ea-a6bf-784f4387d1f7"), log_file_path)
         self._env = UnityToGymWrapper(
             RawUnityEnv(
                 file_path,
                 worker_id,
                 base_port,
                 seed=seed,
-                side_channels=[self.engine_configuration_channel, transform_log_channel],
+                side_channels=[self.engine_configuration_channel, transform_log_channel]
+                if log_file_path is not None
+                else [self.engine_configuration_channel],
             ),
             allow_multiple_obs=False,
         )
