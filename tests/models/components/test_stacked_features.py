a39471e5b2e3080da5bf3f1acb5206753d6199ae
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-08-16 11:05:59 +0900

    変数名を修正

--- tests/models/components/test_stacked_features.py
+++ tests/models/components/test_stacked_features.py
@@ -1,12 +1,12 @@
 import torch
 
-from ami.models.components.stacked_features import LerpStackedFeatures
+from ami.models.components.stacked_features import LerpStackedFeatures, ToStackedFeatures
 
 
 class TestLerpedStackedFeatures:
     def test_forward(self):
         mod = LerpStackedFeatures(128, 64, 8, 4)
 
-        hidden = torch.randn(4, 8, 128)
-        out = mod.forward(hidden)
+        feature = torch.randn(4, 8, 128)
+        out = mod.forward(feature)
         assert out.shape == (4, 64)
