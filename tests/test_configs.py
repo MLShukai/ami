098f70f94b9f09490c96c95e6a3f908efcc6fac1
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-08-28 15:00:11 +0900

    VideoCaptureをmockからfakecameraへ変更

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,5 +1,11 @@
 """ここではconfigファイル上からオブジェクトを正常にインスタンス化可能かテストします。"""
+import logging
+import subprocess
+import sys
+
+import cv2
 import hydra
+import pyfakewebcam
 import pytest
 import torch
 from hydra.utils import instantiate
@@ -12,6 +18,8 @@ from ami.hydra_instantiators import (
 )
 from ami.omegaconf_resolvers import register_custom_resolvers
 from tests.helpers import PROJECT_ROOT
+
+logger = logging.getLogger(__name__)
 
 register_custom_resolvers()
 
@@ -39,7 +47,7 @@ HYDRA_OVERRIDES = [[]] + EXPERIMENT_CONFIG_OVERRIDES
 
 @pytest.mark.parametrize("overrides", HYDRA_OVERRIDES)
 def test_instantiate(overrides: list[str], mocker: MockerFixture, tmp_path):
-    mocker.patch("cv2.VideoCapture")
+    cap = create_fake_camera_if_not_exists()
     mocker.patch("pythonosc.udp_client.SimpleUDPClient")
     with hydra.initialize_config_dir(str(CONFIG_DIR)):
         cfg = hydra.compose(LAUNCH_CONFIG, overrides=overrides + ["devices=cpu"], return_hydra_config=True)
@@ -55,3 +63,32 @@ def test_instantiate(overrides: list[str], mocker: MockerFixture, tmp_path):
         instantiate(threads.main_thread, checkpoint_scheduler=checkpoint_scheduler)
         instantiate(threads.inference_thread, interaction=interaction, data_collectors=data_collectors)
         instantiate(threads.training_thread, models=models, trainers=trainers)
+
+    cap.release()
+
+
+def create_fake_camera_if_not_exists() -> cv2.VideoCapture:
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened():
+        return cap
+
+    if sys.platform == "linux":
+        cmd = ["modprobe", "v4l2loopback", "devices=1"]
+        return_code = subprocess.call(cmd)
+        if return_code != 0:
+            raise RuntimeError(
+                "Failed to create virtual camera device by "
+                + "'"
+                + " ".join(cmd)
+                + "'. return code "
+                + str(return_code)
+            )
+        pyfakewebcam.FakeWebcam("/dev/video0", 1280, 720)
+    else:
+        raise OSError("Can not create virtual camera device on MacOS and Windows.")
+
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened():
+        return cap
+    else:
+        raise RuntimeError("Can not open created camera device...")
