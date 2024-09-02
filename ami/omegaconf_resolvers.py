3bb3136cd16bc1210545cdcc1ba288368780e498
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-09-02 13:29:55 +0900

    globを実行するresolverを追加

--- ami/omegaconf_resolvers.py
+++ ami/omegaconf_resolvers.py
@@ -2,6 +2,7 @@
 
 See: https://omegaconf.readthedocs.io/en/latest/custom_resolvers.html
 """
+import glob
 import os
 import re
 
@@ -22,6 +23,7 @@ def register_custom_resolvers() -> None:
         )  # Usage: ${torch.dtype: float32}
         OmegaConf.register_new_resolver("cvt_time_str", time_string_to_seconds)  # Usage: ${cvt_time_str:"1h"}
         OmegaConf.register_new_resolver("os.cpu_count", os.cpu_count)
+        OmegaConf.register_new_resolver("glob", glob.glob)
 
         _registered = True
 
