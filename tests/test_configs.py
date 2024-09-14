cfad176d640e26e2669a06d4bd67e5b2bf735e40
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-09-14 11:53:52 +0900

    学習済Encoder,Decoderのパラメータファイルを変更

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -37,7 +37,7 @@ if not (DATA_DIR / "random_observation_action_log").exists():
     IGNORE_EXPERIMENT_CONFIGS.add("learn_only_sioconv.yaml")
     IGNORE_EXPERIMENT_CONFIGS.add("learn_i_jepa_sioconv.yaml")
 
-if not (DATA_DIR / "2024-09-07_03-24-12,323203.ckpt").exists():
+if not (DATA_DIR / "2024-09-14_09-42-23,678417.ckpt").exists():
     IGNORE_EXPERIMENT_CONFIGS.add("learn_only_sioconv.yaml")
 
 EXPERIMENT_CONFIG_OVERRIDES = [
