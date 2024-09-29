fd56915e97b38beda2024f4872c47bab014d795d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-09-29 13:56:05 +0900

    instantiate_models_v2を追加

--- ami/hydra_instantiators.py
+++ ami/hydra_instantiators.py
@@ -1,11 +1,15 @@
+import copy
 from typing import Any
 
 import hydra
-from omegaconf import DictConfig, ListConfig
+from omegaconf import DictConfig, ListConfig, open_dict
 
 from .data.buffers.buffer_names import BufferNames
 from .data.utils import BaseDataBuffer, DataCollectorsDict, ThreadSafeDataCollector
 from .logger import get_main_thread_logger
+from .models.instantiations import (
+    InstantiationReturnType as ModelInstantiationReturnType,
+)
 from .models.model_names import ModelNames
 from .models.utils import ModelWrapper, ModelWrappersDict
 from .trainers.utils import BaseTrainer, TrainersList
@@ -33,11 +37,57 @@ def instantiate_data_collectors(data_collectors_cfg: DictConfig | None) -> DataC
 
 
 def instantiate_models(models_cfg: DictConfig | None) -> ModelWrappersDict:
-    d = ModelWrappersDict()
+    """Models (ModelWrappersDict)をインスタンス化する。
+
+    `model_cfg`がNoneであれば、空のModelWrappersDictを返す。
+
+    * Instantiation Versionについて
+        `_version_` を`models_cfg`に追加することでインスタンス化方法のバージョンを切り替えることができる。
+        指定がない場合はデフォルトで 1 である。
+
+        * version 1の場合のconfig 構造
+            ```yaml
+            <model_name>:
+              _target_: path.to.model_wrapper
+              default_device: ...
+              model:
+                _target_: path.to.model.class
+                arg1: ...
+            ...
+            ```
+
+        * version 2 の場合の config 構造
+            ```yaml
+            _version_: 2
+
+            _target_: ami.models.instantiations.<target>
+            arg1: ...
+            ```
+            インスタンス化関数の記述ルールに関しては `ami/models/instantiations.py`を参照
+    """
     if models_cfg is None:
         logger.info("No model configs are provided.")
-        return d
+        return ModelWrappersDict()
 
+    version = 1
+    if "_version_" in models_cfg:
+        models_cfg = copy.copy(models_cfg)
+        with open_dict(models_cfg):
+            version = models_cfg.pop("_version_")
+
+    match version:
+        case 1:
+            logger.info("Model Instantiation Version is 1.")
+            return instantiate_models_v1(models_cfg)
+        case 2:
+            logger.info("Model Instantiation Version is 2.")
+            return instantiate_models_v2(models_cfg)
+        case _:
+            raise ValueError(f"Specified instantiation version is invalid: {version!r}")
+
+
+def instantiate_models_v1(models_cfg: DictConfig) -> ModelWrappersDict:
+    d = ModelWrappersDict()
     aliases = []
     for name, cfg_or_alias_target in models_cfg.items():
         match cfg_or_alias_target:
@@ -54,6 +104,32 @@ def instantiate_models(models_cfg: DictConfig | None) -> ModelWrappersDict:
                 raise RuntimeError(f"Model config or alias must be DictConfig or str! {cfg}")
     for alias, target in aliases:
         d[ModelNames(str(alias))] = d[ModelNames(str(target))]
+    return d
+
+
+def instantiate_models_v2(model_cfg: DictConfig) -> ModelWrappersDict:
+    d = ModelWrappersDict()
+    logger.info(f"Instantiating models <{model_cfg._target_}>")
+    models: ModelInstantiationReturnType = hydra.utils.instantiate(model_cfg)
+
+    aliases = []
+    for name, model_wrapper_or_alias in models.items():
+        match model_wrapper_or_alias:
+            case ModelWrapper():
+                model_wrapper = model_wrapper_or_alias
+                logger.info(
+                    f"Instantiated model {name!r}: <{model_wrapper.__class__.__name__}[{model_wrapper.model.__class__.__name__}]>"
+                )
+                d[name] = model_wrapper
+            case ModelNames():
+                target = model_wrapper_or_alias
+                logger.info(f"Model name {name!r} is alias to {target!r}")
+                aliases.append((name, target))
+            case _:
+                raise ValueError(f"Invalid model wrapper or alias type!: {model_wrapper_or_alias!r}")
+
+    for alias, target in aliases:
+        d[alias] = d[target]
     return d
 
 
--- tests/test_hydra_instantiators.py
+++ tests/test_hydra_instantiators.py
@@ -1,11 +1,16 @@
 import pytest
 from omegaconf import OmegaConf
 
-from ami.hydra_instantiators import instantiate_models
-from ami.models.model_names import ModelNames
+from ami.hydra_instantiators import (
+    ModelNames,
+    ModelWrapper,
+    instantiate_models,
+    instantiate_models_v1,
+    instantiate_models_v2,
+)
 
 
-def test_instantiate_models():
+def test_instantiate_models_v1():
     # Test instantiate
     cfg = OmegaConf.create(
         """\
@@ -19,7 +24,7 @@ def test_instantiate_models():
             latent_dim: 512
         """
     )
-    models = instantiate_models(cfg)
+    models = instantiate_models_v1(cfg)
     assert ModelNames.IMAGE_DECODER in models
 
     # Test alias
@@ -36,7 +41,7 @@ def test_instantiate_models():
         image_encoder: image_decoder
         """
     )
-    models = instantiate_models(cfg)
+    models = instantiate_models_v1(cfg)
     assert ModelNames.IMAGE_DECODER in models
     assert ModelNames.IMAGE_ENCODER in models
     assert models[ModelNames.IMAGE_ENCODER] is models[ModelNames.IMAGE_DECODER]
@@ -56,4 +61,98 @@ def test_instantiate_models():
         """
     )
     with pytest.raises(RuntimeError):
-        instantiate_models(cfg)
+        instantiate_models_v1(cfg)
+
+
+def instantiate_func_for_v2(height, width, channels, latent_dim):
+    from ami.models.vae import Conv2dEncoder
+
+    return {
+        ModelNames.I_JEPA_CONTEXT_ENCODER: ModelNames.IMAGE_ENCODER,  # Alias
+        ModelNames.IMAGE_ENCODER: ModelWrapper(Conv2dEncoder(height, width, channels, latent_dim)),
+    }
+
+
+def test_instantiate_models_v2():
+    # Test instantiate
+    cfg = OmegaConf.create(
+        """\
+        _target_: tests.test_hydra_instantiators.instantiate_func_for_v2
+        height: 84
+        width: 84
+        channels: 3
+        latent_dim: 512
+        """
+    )
+
+    models = instantiate_models_v2(cfg)
+    assert ModelNames.IMAGE_ENCODER in models
+    assert ModelNames.I_JEPA_CONTEXT_ENCODER in models
+    assert models[ModelNames.IMAGE_ENCODER] is models[ModelNames.I_JEPA_CONTEXT_ENCODER]
+
+
+def test_instantiate_models():
+
+    # No config
+    models = instantiate_models(None)
+    assert len(models.items()) == 0
+
+    # Test instantiate v1
+    cfg = OmegaConf.create(
+        """\
+        image_decoder:
+          _target_: ami.models.model_wrapper.ModelWrapper
+          model:
+            _target_: ami.models.vae.Conv2dDecoder
+            height: 84
+            width: 84
+            channels: 3
+            latent_dim: 512
+        """
+    )
+    models = instantiate_models(cfg)
+    assert ModelNames.IMAGE_DECODER in models
+
+    # Explicit versioning
+    cfg = OmegaConf.create(
+        """\
+        _version_: 1
+        image_decoder:
+          _target_: ami.models.model_wrapper.ModelWrapper
+          model:
+            _target_: ami.models.vae.Conv2dDecoder
+            height: 84
+            width: 84
+            channels: 3
+            latent_dim: 512
+        """
+    )
+    models = instantiate_models(cfg)
+    assert ModelNames.IMAGE_DECODER in models
+    assert "_version_" in cfg
+
+    # Test instantiate v2
+    cfg = OmegaConf.create(
+        """
+        _version_: 2
+        _target_: tests.test_hydra_instantiators.instantiate_func_for_v2
+        height: 84
+        width: 84
+        channels: 3
+        latent_dim: 512
+        """
+    )
+
+    models = instantiate_models(cfg)
+    assert ModelNames.IMAGE_ENCODER in models
+    assert "_version_" in cfg  # not modified
+
+    # Test error with invalid version value.
+    with pytest.raises(ValueError):
+        instantiate_models(OmegaConf.create("_version_: null"))
+
+    with pytest.raises(ValueError):
+        instantiate_models(OmegaConf.create("_version_: 0"))
+
+    with pytest.raises(ValueError):
+        instantiate_models(OmegaConf.create("_version_: one"))
