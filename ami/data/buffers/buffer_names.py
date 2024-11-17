216ac60f39b8e0f18842ab885238a4c8da798b5b
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-11-17 10:50:51 +0900

    add audio modality

--- ami/data/buffers/buffer_names.py
+++ ami/data/buffers/buffer_names.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class BufferNames(str, Enum):
     IMAGE = "image"
+    AUDIO = "audio"
     PPO_TRAJECTORY = "ppo_trajectory"
     FORWARD_DYNAMICS_TRAJECTORY = "forward_dynamics_trajectory"
     DREAMING_INITIAL_STATES = "dreaming_initial_states"
