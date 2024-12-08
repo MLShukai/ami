869408457cff34eb3202d4d59a5e37ea8db9f459
zassou65535 <gravity@kuf.biglobe.ne.jp>
2024-12-08 19:40:32 +0900

    MultiScaleDiscriminatorに関するTest追加

--- ami/models/hifigan_discriminators.py
+++ ami/models/hifigan_discriminators.py
@@ -6,38 +6,41 @@ import logging
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.parametrizations import spectral_norm, weight_norm
 
-from torch.nn.utils.parametrizations import weight_norm, spectral_norm
 
 def get_padding(kernel_size: int, dilation: int = 1) -> int:
     return int((kernel_size * dilation - dilation) / 2)
 
+
 class DiscriminatorP(nn.Module):
     def __init__(
-        self, 
-        period: int, 
-        in_channels: int=1, 
-        kernel_size: int=5, 
-        stride: int=3,
+        self,
+        period: int,
+        in_channels: int = 1,
+        kernel_size: int = 5,
+        stride: int = 3,
     ) -> None:
-        super(DiscriminatorP, self).__init__()
+        super().__init__()
         self.period = period
-        self.convs = nn.ModuleList([
-            weight_norm(nn.Conv2d(in_channels, 32, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
-            weight_norm(nn.Conv2d(32, 128, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
-            weight_norm(nn.Conv2d(128, 512, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
-            weight_norm(nn.Conv2d(512, 1024, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
-            weight_norm(nn.Conv2d(1024, 1024, (kernel_size, 1), 1, padding=(2, 0))),
-        ])
+        self.convs = nn.ModuleList(
+            [
+                weight_norm(nn.Conv2d(in_channels, 32, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+                weight_norm(nn.Conv2d(32, 128, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+                weight_norm(nn.Conv2d(128, 512, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+                weight_norm(nn.Conv2d(512, 1024, (kernel_size, 1), (stride, 1), padding=(get_padding(5, 1), 0))),
+                weight_norm(nn.Conv2d(1024, 1024, (kernel_size, 1), 1, padding=(2, 0))),
+            ]
+        )
         self.conv_post = weight_norm(nn.Conv2d(1024, 1, (3, 1), 1, padding=(1, 0)))
 
     def forward(self, input_waveforms: torch.Tensor) -> tuple[torch.Tensor, list[torch.Tensor]]:
         fmaps: list[torch.Tesnor] = []
         x = input_waveforms
-        
+
         # 1d to 2d
         batch_size, channels, sample_size = x.shape
-        if sample_size % self.period != 0: # pad first
+        if sample_size % self.period != 0:  # pad first
             n_pad = self.period - (sample_size % self.period)
             x = F.pad(x, (0, n_pad), "reflect")
             sample_size = sample_size + n_pad
@@ -55,24 +58,21 @@ class DiscriminatorP(nn.Module):
 
 
 class MultiPeriodDiscriminator(nn.Module):
-    def __init__(self, in_channels: int=1) -> None:
-        super(MultiPeriodDiscriminator, self).__init__()
-        self.discriminators = nn.ModuleList([
-            DiscriminatorP(period=2, in_channels=in_channels),
-            DiscriminatorP(period=3, in_channels=in_channels),
-            DiscriminatorP(period=5, in_channels=in_channels),
-            DiscriminatorP(period=7, in_channels=in_channels),
-            DiscriminatorP(period=11, in_channels=in_channels),
-        ])
+    def __init__(self, in_channels: int = 1) -> None:
+        super().__init__()
+        self.discriminators = nn.ModuleList(
+            [
+                DiscriminatorP(period=2, in_channels=in_channels),
+                DiscriminatorP(period=3, in_channels=in_channels),
+                DiscriminatorP(period=5, in_channels=in_channels),
+                DiscriminatorP(period=7, in_channels=in_channels),
+                DiscriminatorP(period=11, in_channels=in_channels),
+            ]
+        )
 
     def forward(
         self, real_waveforms: torch.Tensor, fake_waveforms: torch.Tensor
-    ) -> tuple[
-        list[torch.Tensor], 
-        list[torch.Tensor],
-        list[list[torch.Tensor]], 
-        list[list[torch.Tensor]],
-    ]:
+    ) -> tuple[list[torch.Tensor], list[torch.Tensor], list[list[torch.Tensor]], list[list[torch.Tensor]]]:
         y_d_rs: list[torch.Tensor] = []
         y_d_fs: list[torch.Tensor] = []
         fmaps_rs: list[list[torch.Tensor]] = []
@@ -88,18 +88,20 @@ class MultiPeriodDiscriminator(nn.Module):
 
 
 class DiscriminatorS(nn.Module):
-    def __init__(self, in_channels: int=1, use_spectral_norm: bool=False) -> None:
-        super(DiscriminatorS, self).__init__()
-        norm_f = weight_norm if use_spectral_norm == False else spectral_norm
-        self.convs = nn.ModuleList([
-            norm_f(nn.Conv1d(in_channels, 128, 15, 1, padding=7)),
-            norm_f(nn.Conv1d(128, 128, 41, 2, groups=4, padding=20)),
-            norm_f(nn.Conv1d(128, 256, 41, 2, groups=16, padding=20)),
-            norm_f(nn.Conv1d(256, 512, 41, 4, groups=16, padding=20)),
-            norm_f(nn.Conv1d(512, 1024, 41, 4, groups=16, padding=20)),
-            norm_f(nn.Conv1d(1024, 1024, 41, 1, groups=16, padding=20)),
-            norm_f(nn.Conv1d(1024, 1024, 5, 1, padding=2)),
-        ])
+    def __init__(self, in_channels: int = 1, use_spectral_norm: bool = False) -> None:
+        super().__init__()
+        norm_f = spectral_norm if use_spectral_norm else weight_norm
+        self.convs = nn.ModuleList(
+            [
+                norm_f(nn.Conv1d(in_channels, 128, 15, 1, padding=7)),
+                norm_f(nn.Conv1d(128, 128, 41, 2, groups=4, padding=20)),
+                norm_f(nn.Conv1d(128, 256, 41, 2, groups=16, padding=20)),
+                norm_f(nn.Conv1d(256, 512, 41, 4, groups=16, padding=20)),
+                norm_f(nn.Conv1d(512, 1024, 41, 4, groups=16, padding=20)),
+                norm_f(nn.Conv1d(1024, 1024, 41, 1, groups=16, padding=20)),
+                norm_f(nn.Conv1d(1024, 1024, 5, 1, padding=2)),
+            ]
+        )
         self.conv_post = norm_f(nn.Conv1d(1024, 1, 3, 1, padding=1))
 
     def forward(self, input_waveforms: torch.Tensor) -> tuple[torch.Tensor, list[torch.Tensor]]:
@@ -116,26 +118,20 @@ class DiscriminatorS(nn.Module):
 
 
 class MultiScaleDiscriminator(nn.Module):
-    def __init__(self, in_channels: int=1) -> None:
-        super(MultiScaleDiscriminator, self).__init__()
-        self.discriminators = nn.ModuleList([
-            DiscriminatorS(in_channels=in_channels, use_spectral_norm=True),
-            DiscriminatorS(in_channels=in_channels),
-            DiscriminatorS(in_channels=in_channels),
-        ])
-        self.meanpools = nn.ModuleList([
-            AvgPool1d(4, 2, padding=2),
-            AvgPool1d(4, 2, padding=2)
-        ])
+    def __init__(self, in_channels: int = 1) -> None:
+        super().__init__()
+        self.discriminators = nn.ModuleList(
+            [
+                DiscriminatorS(in_channels=in_channels, use_spectral_norm=True),
+                DiscriminatorS(in_channels=in_channels),
+                DiscriminatorS(in_channels=in_channels),
+            ]
+        )
+        self.meanpools = nn.ModuleList([nn.AvgPool1d(4, 2, padding=2), nn.AvgPool1d(4, 2, padding=2)])
 
     def forward(
         self, real_waveforms: torch.Tensor, fake_waveforms: torch.Tensor
-    ) -> tuple[
-        list[torch.Tensor],
-        list[torch.Tensor],
-        list[list[torch.Tensor]],
-        list[list[torch.Tensor]],
-    ]:
+    ) -> tuple[list[torch.Tensor], list[torch.Tensor], list[list[torch.Tensor]], list[list[torch.Tensor]]]:
         y = real_waveforms
         y_hat = fake_waveforms
         y_d_rs: list[torch.Tensor] = []
@@ -144,8 +140,8 @@ class MultiScaleDiscriminator(nn.Module):
         fmaps_fs: list[list[torch.Tensor]] = []
         for i, discriminator in enumerate(self.discriminators):
             if i != 0:
-                y = self.meanpools[i-1](y)
-                y_hat = self.meanpools[i-1](y_hat)
+                y = self.meanpools[i - 1](y)
+                y_hat = self.meanpools[i - 1](y_hat)
             y_d_r, fmaps_r = discriminator(y)
             y_d_g, fmaps_g = discriminator(y_hat)
             y_d_rs.append(y_d_r)
