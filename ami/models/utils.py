a212bce491a6885523d0dc4274c046389096d0f0
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2024-12-25 13:21:37 +0900

    fix type converting

--- ami/models/utils.py
+++ ami/models/utils.py
@@ -171,4 +171,4 @@ size_2d: TypeAlias = int | tuple[int, int]
 
 def size_2d_to_int_tuple(size: size_2d) -> tuple[int, int]:
     """Convert `size_2d` type to int tuple."""
-    return (size, size) if isinstance(size, int) else tuple(size)
+    return (size, size) if isinstance(size, int) else (size[0], size[1])
