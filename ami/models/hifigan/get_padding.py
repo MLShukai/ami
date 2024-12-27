170383c8e9dd9dca72eabb7cc2b842e6d9d56b95
zassou65535 <gravity@kuf.biglobe.ne.jp>
2024-12-27 17:55:05 +0900

    get_padding関数を分離

---
+++ ami/models/hifigan/get_padding.py
@@ -0,0 +1,5 @@
+# Ref:
+# https://github.com/jik876/hifi-gan/blob/master/models.py
+
+def get_padding(kernel_size: int, dilation: int = 1) -> int:
+    return int((kernel_size * dilation - dilation) / 2)
