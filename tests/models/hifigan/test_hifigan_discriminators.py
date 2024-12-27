a057971078ce819c91b55bce71bfe5ca6e7a33f5
zassou65535 <gravity@kuf.biglobe.ne.jp>
2024-12-27 15:05:49 +0900

    hifigan関係をまとめる

--- tests/models/hifigan/test_hifigan_discriminators.py
+++ tests/models/hifigan/test_hifigan_discriminators.py
@@ -3,7 +3,7 @@ import math
 import pytest
 import torch
 
-from ami.models.hifigan_discriminators import (
+from ami.models.hifigan.hifigan_discriminators import (
     MultiPeriodDiscriminator,
     MultiScaleDiscriminator,
 )
