13ca7b6f9417dcf1b33a3a775a3a3e03d937db52
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-01-17 20:36:21 +0900

    discriminatorのchannel数を引数から指定できるよう変更

--- ami/models/hifigan/discriminators.py
+++ ami/models/hifigan/discriminators.py
@@ -22,6 +22,7 @@ class PeriodDiscriminator(nn.Module):
         self,
         period: int,
         in_channels: int = 1,
+        initial_hidden_channels: int = 32,  # referenced from discriminator channels in original impl.
         kernel_size: int = 5,
         stride: int = 3,
     ) -> None:
@@ -29,11 +30,11 @@ class PeriodDiscriminator(nn.Module):
         self.period = period
 
         channels = [
-            (in_channels, 32),
-            (32, 128),
-            (128, 512),
-            (512, 1024),
-            (1024, 1024),
+            (in_channels, initial_hidden_channels),
+            (initial_hidden_channels, initial_hidden_channels*4),
+            (initial_hidden_channels*4, initial_hidden_channels*16),
+            (initial_hidden_channels*16, initial_hidden_channels*32),
+            (initial_hidden_channels*32, initial_hidden_channels*32),
         ]
         self.convs = nn.ModuleList()
         for i, (in_ch, out_ch) in enumerate(channels, start=1):
@@ -94,11 +95,20 @@ class PeriodDiscriminator(nn.Module):
 class MultiPeriodDiscriminator(nn.Module):
     """Discriminate authenticity of the periodicity of the input audio."""
 
-    def __init__(self, in_channels: int = 1) -> None:
+    def __init__(
+        self,
+        in_channels: int = 1,
+        initial_hidden_channels: int = 32,  # referenced from discriminator channels in original impl.
+    ) -> None:
         super().__init__()
         periods = [2, 3, 5, 7, 11]
         self.discriminators = nn.ModuleList(
-            [PeriodDiscriminator(period=period, in_channels=in_channels) for period in periods]
+            [
+                PeriodDiscriminator(
+                    period=period, in_channels=in_channels, initial_hidden_channels=initial_hidden_channels
+                )
+                for period in periods
+            ]
         )
 
     def forward(self, input_waveforms: torch.Tensor) -> tuple[list[torch.Tensor], list[list[torch.Tensor]]]:
@@ -134,21 +144,26 @@ class ScaleDiscriminator(nn.Module):
     consecutively evaluate the audio sequence.
     """
 
-    def __init__(self, in_channels: int = 1, use_spectral_norm: bool = False) -> None:
+    def __init__(
+        self,
+        in_channels: int = 1,
+        initial_hidden_channels: int = 128,  # referenced from discriminator channels in original impl.
+        use_spectral_norm: bool = False,
+    ) -> None:
         super().__init__()
         norm_f = spectral_norm if use_spectral_norm else weight_norm
         self.convs = nn.ModuleList(
             [
-                norm_f(nn.Conv1d(in_channels, 128, 15, 1, padding=7)),
-                norm_f(nn.Conv1d(128, 128, 41, 2, groups=4, padding=20)),
-                norm_f(nn.Conv1d(128, 256, 41, 2, groups=16, padding=20)),
-                norm_f(nn.Conv1d(256, 512, 41, 4, groups=16, padding=20)),
-                norm_f(nn.Conv1d(512, 1024, 41, 4, groups=16, padding=20)),
-                norm_f(nn.Conv1d(1024, 1024, 41, 1, groups=16, padding=20)),
-                norm_f(nn.Conv1d(1024, 1024, 5, 1, padding=2)),
+                norm_f(nn.Conv1d(in_channels, initial_hidden_channels, 15, 1, padding=7)),
+                norm_f(nn.Conv1d(initial_hidden_channels, initial_hidden_channels, 41, 2, groups=4, padding=20)),
+                norm_f(nn.Conv1d(initial_hidden_channels, initial_hidden_channels*2, 41, 2, groups=16, padding=20)),
+                norm_f(nn.Conv1d(initial_hidden_channels*2, initial_hidden_channels*4, 41, 4, groups=16, padding=20)),
+                norm_f(nn.Conv1d(initial_hidden_channels*4, initial_hidden_channels*8, 41, 4, groups=16, padding=20)),
+                norm_f(nn.Conv1d(initial_hidden_channels*8, initial_hidden_channels*8, 41, 1, groups=16, padding=20)),
+                norm_f(nn.Conv1d(initial_hidden_channels*8, initial_hidden_channels*8, 5, 1, padding=2)),
             ]
         )
-        self.conv_post = norm_f(nn.Conv1d(1024, 1, 3, 1, padding=1))
+        self.conv_post = norm_f(nn.Conv1d(initial_hidden_channels*8, 1, 3, 1, padding=1))
 
     def forward(self, input_waveforms: torch.Tensor) -> tuple[torch.Tensor, list[torch.Tensor]]:
         """Discriminate authenticity of the input audio consecutively.
@@ -181,13 +196,19 @@ class MultiScaleDiscriminator(nn.Module):
     accepts disjoint samples, Adding MultiScaleDiscriminator to consecutively
     evaluate the audio sequence."""
 
-    def __init__(self, in_channels: int = 1) -> None:
+    def __init__(
+        self,
+        in_channels: int = 1,
+        initial_hidden_channels: int = 128,  # referenced from discriminator channels in original impl.
+    ) -> None:
         super().__init__()
         self.discriminators = nn.ModuleList(
             [
-                ScaleDiscriminator(in_channels=in_channels, use_spectral_norm=True),
-                ScaleDiscriminator(in_channels=in_channels),
-                ScaleDiscriminator(in_channels=in_channels),
+                ScaleDiscriminator(
+                    in_channels=in_channels, initial_hidden_channels=initial_hidden_channels, use_spectral_norm=True
+                ),
+                ScaleDiscriminator(in_channels=in_channels, initial_hidden_channels=initial_hidden_channels),
+                ScaleDiscriminator(in_channels=in_channels, initial_hidden_channels=initial_hidden_channels),
             ]
         )
         self.meanpools = nn.AvgPool1d(kernel_size=4, stride=2, padding=2)
