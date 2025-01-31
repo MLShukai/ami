ceb993f3d4ecf627a274b6632190967127ecf5c3
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-01-31 15:23:54 +0900

    Change default file name format from WAV to MP3 in TensorAudioRecorder

--- ami/interactions/io_wrappers/tensor_audio_recorder.py
+++ ami/interactions/io_wrappers/tensor_audio_recorder.py
@@ -21,7 +21,7 @@ class TensorAudioRecorder(BaseIOWrapper[Tensor, Tensor]):
         output_dir: str | Path,
         sample_rate: float,
         channel_size: int,
-        file_name_format: str = "%Y-%m-%d_%H-%M-%S.%f.wav",
+        file_name_format: str = "%Y-%m-%d_%H-%M-%S.%f.mp3",
         sample_slice_size: int | None = None,
     ) -> None:
         """Initializes the TensorAudioRecorder.
