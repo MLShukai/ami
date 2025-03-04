2a3cec1fe047ceb4c188aa0b364e446ec0c58666
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-03-04 15:49:44 +0900

    Add PrimitiveForwardDynamics Module

--- ami/models/forward_dynamics.py
+++ ami/models/forward_dynamics.py
@@ -62,3 +62,36 @@ class ForwardDynamcisWithActionReward(nn.Module):
         action_hat_dist = self.action_hat_dist_head(x)
         reward_hat_dist = self.reward_hat_dist_head(x)
         return obs_hat_dist, action_hat_dist, reward_hat_dist, next_hidden
+
+
+class PrimitiveForwardDynamics(nn.Module):
+    """Primitive forward dynamics model.
+
+    f: state, action -> next_state, next_action
+    """
+
+    def __init__(
+        self,
+        observation_flatten: nn.Module,
+        action_flatten: nn.Module,
+        obs_action_projection: nn.Module,
+        core_model: nn.Module,
+        obs_hat_dist_head: nn.Module,
+        action_hat_dist_head: nn.Module,
+    ) -> None:
+        super().__init__()
+        self.observation_flatten = observation_flatten
+        self.action_flatten = action_flatten
+        self.obs_action_projection = obs_action_projection
+        self.core_model = core_model
+        self.obs_hat_dist_head = obs_hat_dist_head
+        self.action_hat_dist_head = action_hat_dist_head
+
+    def forward(self, obs: Tensor, action: Tensor) -> tuple[Distribution, Distribution]:
+        obs_flat = self.observation_flatten(obs)
+        action_flat = self.action_flatten(action)
+        x = self.obs_action_projection(torch.cat((obs_flat, action_flat), dim=-1))
+        x = self.core_model(x)
+        obs_hat_dist = self.obs_hat_dist_head(x)
+        action_hat_dist = self.action_hat_dist_head(x)
+        return obs_hat_dist, action_hat_dist
--- tests/models/test_forward_dynamics.py
+++ tests/models/test_forward_dynamics.py
@@ -6,7 +6,11 @@ from ami.models.components.fully_connected_fixed_std_normal import (
     FullyConnectedFixedStdNormal,
 )
 from ami.models.components.sconv import SConv
-from ami.models.forward_dynamics import ForwardDynamcisWithActionReward, ForwardDynamics
+from ami.models.forward_dynamics import (
+    ForwardDynamcisWithActionReward,
+    ForwardDynamics,
+    PrimitiveForwardDynamics,
+)
 
 BATCH = 4
 DEPTH = 8
@@ -130,3 +134,58 @@ class TestForwardDynamicsWithActionReward:
         assert action_hat_dist.sample().shape == action_shape
         assert reward_dist.sample().shape == (LEN, 1)
         assert hidden.shape == hidden_shape
+
+
+class TestPrimitiveForwardDynamics:
+    @pytest.fixture
+    def core_model(self):
+        return nn.Linear(DIM, DIM)
+
+    @pytest.fixture
+    def observation_flatten(self):
+        return nn.Identity()
+
+    @pytest.fixture
+    def action_flatten(self):
+        return nn.Identity()
+
+    @pytest.fixture
+    def obs_action_projection(self):
+        return nn.Linear(DIM_OBS + DIM_ACTION, DIM)
+
+    @pytest.fixture
+    def obs_hat_dist_head(self):
+        return FullyConnectedFixedStdNormal(DIM, DIM_OBS)
+
+    @pytest.fixture
+    def action_hat_dist_head(self):
+        return FullyConnectedFixedStdNormal(DIM, DIM_ACTION)
+
+    @pytest.fixture
+    def forward_dynamics(
+        self,
+        observation_flatten,
+        action_flatten,
+        obs_action_projection,
+        core_model,
+        obs_hat_dist_head,
+        action_hat_dist_head,
+    ):
+        return PrimitiveForwardDynamics(
+            observation_flatten,
+            action_flatten,
+            obs_action_projection,
+            core_model,
+            obs_hat_dist_head,
+            action_hat_dist_head,
+        )
+
+    def test_forward_dynamycs(self, forward_dynamics):
+        obs_shape = (LEN, DIM_OBS)
+        obs = torch.randn(*obs_shape)
+        action_shape = (LEN, DIM_ACTION)
+        action = torch.randn(*action_shape)
+
+        obs_hat_dist, action_hat_dist = forward_dynamics(obs, action)
+        assert obs_hat_dist.sample().shape == obs_shape
+        assert action_hat_dist.sample().shape == action_shape
