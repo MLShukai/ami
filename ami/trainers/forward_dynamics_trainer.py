a24489b071a533b6db3137edb683833ffe362b3c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-03-05 11:46:05 +0900

    Add PrimitiveForwardDynamicsTrainer

--- ami/trainers/forward_dynamics_trainer.py
+++ ami/trainers/forward_dynamics_trainer.py
@@ -12,7 +12,11 @@ from typing_extensions import override
 from ami.data.buffers.buffer_names import BufferNames
 from ami.data.buffers.causal_data_buffer import CausalDataBuffer
 from ami.data.interfaces import ThreadSafeDataUser
-from ami.models.forward_dynamics import ForwardDynamcisWithActionReward, ForwardDynamics
+from ami.models.forward_dynamics import (
+    ForwardDynamcisWithActionReward,
+    ForwardDynamics,
+    PrimitiveForwardDynamics,
+)
 from ami.models.model_names import ModelNames
 from ami.models.model_wrapper import ModelWrapper
 from ami.tensorboard_loggers import StepIntervalLogger
@@ -274,6 +278,120 @@ class ForwardDynamicsWithActionRewardTrainer(BaseTrainer):
                     torch.nn.utils.clip_grad_norm_(
                         self.forward_dynamics.parameters(), self.gradient_clip_norm, error_if_nonfinite=True
                     )
+                optimizer.step()
+                self.logger.update()
+
+        self.optimizer_state = optimizer.state_dict()
+
+    @override
+    def save_state(self, path: Path) -> None:
+        path.mkdir()
+        torch.save(self.optimizer_state, path / "optimizer.pt")
+        torch.save(self.logger.state_dict(), path / "logger.pt")
+        torch.save(self.dataset_previous_get_time, path / "dataset_previous_get_time.pt")
+
+    @override
+    def load_state(self, path: Path) -> None:
+        self.optimizer_state = torch.load(path / "optimizer.pt")
+        self.logger.load_state_dict(torch.load(path / "logger.pt"))
+        self.dataset_previous_get_time = torch.load(path / "dataset_previous_get_time.pt")
+
+
+class PrimitiveForwardDynamicsTrainer(BaseTrainer):
+    def __init__(
+        self,
+        partial_dataloader: partial[DataLoader[torch.Tensor]],
+        partial_sampler: partial[RandomTimeSeriesSampler],
+        partial_optimizer: partial[Optimizer],
+        device: torch.device,
+        logger: StepIntervalLogger,
+        max_epochs: int = 1,
+        minimum_new_data_count: int = 2,
+    ) -> None:
+        """Initialization.
+
+        Args:
+            partial_dataloader: A partially instantiated dataloader lacking a provided dataset.
+            partial_sampler: A partially instantiated sampler lacking a provided dataset.
+            partial_optimizer: A partially instantiated optimizer lacking provided parameters.
+            device: The accelerator device (e.g., CPU, GPU) utilized for training the model.
+            minimum_new_data_count: Minimum number of new data count required to run the training.
+        """
+        super().__init__()
+        self.partial_optimizer = partial_optimizer
+        self.partial_dataloader = partial_dataloader
+        self.partial_sampler = partial_sampler
+        self.device = device
+        self.logger = logger
+        self.max_epochs = max_epochs
+        assert minimum_new_data_count >= 2, "minimum_new_data_count must be at least 2"
+        self.minimum_new_data_count = minimum_new_data_count
+        self.dataset_previous_get_time = float("-inf")
+
+    def on_data_users_dict_attached(self) -> None:
+        self.trajectory_data_user: ThreadSafeDataUser[CausalDataBuffer] = self.get_data_user(
+            BufferNames.FORWARD_DYNAMICS_TRAJECTORY
+        )
+
+    def on_model_wrappers_dict_attached(self) -> None:
+        self.forward_dynamics: ModelWrapper[PrimitiveForwardDynamics] = self.get_training_model(
+            ModelNames.FORWARD_DYNAMICS
+        )
+        self.optimizer_state = self.partial_optimizer(self.forward_dynamics.parameters()).state_dict()
+
+    def is_trainable(self) -> bool:
+        self.trajectory_data_user.update()
+        return self._is_new_data_available()
+
+    def _is_new_data_available(self) -> bool:
+        return (
+            self.trajectory_data_user.buffer.count_data_added_since(self.dataset_previous_get_time)
+            >= self.minimum_new_data_count
+        )
+
+    def get_dataset(self) -> Dataset[Tensor]:
+        dataset = self.trajectory_data_user.get_dataset()
+        self.dataset_previous_get_time = time.time()
+        return dataset
+
+    def train(self) -> None:
+        self.forward_dynamics.to(self.device)
+
+        optimizer = self.partial_optimizer(self.forward_dynamics.parameters())
+        optimizer.load_state_dict(self.optimizer_state)
+
+        dataset = self.get_dataset()
+        sampler = self.partial_sampler(dataset)
+        dataloader = self.partial_dataloader(dataset=dataset, sampler=sampler)
+
+        for _ in range(self.max_epochs):
+            for batch in dataloader:
+                observations, actions = batch
+                observations = observations.to(self.device)
+                actions = actions.to(self.device)
+
+                observations, actions, observations_next = (
+                    observations[:, :-1],  # o_0:T-1
+                    actions[:, :-1],  # a_0:T-1
+                    observations[:, 1:],  # o_1:T
+                )
+
+                optimizer.zero_grad()
+
+                observations_next_hat_dist: Distribution
+                observations_next_hat_dist = self.forward_dynamics(observations, actions)
+
+                loss = -observations_next_hat_dist.log_prob(observations_next).mean()
+
+                prefix = "forward_dynamics/"
+                self.logger.log(prefix + "loss", loss)
+
+                loss.backward()
+
+                grad_norm = grad_norm = torch.cat(
+                    [p.grad.flatten() for p in self.forward_dynamics.parameters() if p.grad is not None]
+                ).norm()
+                self.logger.log(prefix + "metrics/grad_norm", grad_norm)
                 optimizer.step()
                 self.logger.update()
 
