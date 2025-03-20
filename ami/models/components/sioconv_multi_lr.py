8e185d63a1f3844822ce7bf42d8de3011c6c4174
myxyy <m.myxomatosis@gmail.com>
2025-03-20 18:38:04 +0900

    num_headを削除

--- ami/models/components/sioconv_multi_lr.py
+++ ami/models/components/sioconv_multi_lr.py
@@ -1,38 +1,34 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
-import numpy as np
 
 from .sioconvps import RMSNorm, SioConvPSLayer
 from .stacked_hidden_state import StackedHiddenState
 
+
 class MultiLRMLP(nn.Module):
-    def __init__(self, dim: int, dim_ff_hidden: int, num_head: int, lr_scale: float, weight_decay: float, activation: nn.Module=nn.SiLU()):
+    def __init__(
+        self, dim: int, dim_ff_hidden: int, lr_scale: float, weight_decay: float, activation: nn.Module = nn.SiLU()
+    ):
         super().__init__()
-        assert dim_ff_hidden % num_head == 0
-        head_dim = dim_ff_hidden // num_head
         self.fc1_weight = nn.Parameter(torch.randn(dim_ff_hidden, dim))
         self.fc1_bias = nn.Parameter(torch.zeros(dim_ff_hidden))
         self.fc2_weight = nn.Parameter(torch.randn(dim, dim_ff_hidden))
         self.fc2_bias = nn.Parameter(torch.zeros(dim))
         self.activation = activation
 
-        self.lr_scale = torch.exp(torch.linspace(0, np.log(lr_scale), num_head))
-        self.weight_decay = torch.exp(np.log(weight_decay) + torch.linspace(-np.log(lr_scale), 0, num_head))
+        self.lr_scale = torch.exp(torch.linspace(0, np.log(lr_scale), dim_ff_hidden))
+        self.weight_decay = torch.exp(np.log(weight_decay) + torch.linspace(-np.log(lr_scale), 0, dim_ff_hidden))
 
-        self.fc1_weight.register_hook(lambda grad: (
-            grad.view(num_head, head_dim, dim).mul_(self.lr_scale[:,None,None]) +
-            self.fc1_weight.view(num_head, head_dim, dim).mul_(self.weight_decay[:,None,None]))
-        .view(dim_ff_hidden, dim))
-        self.fc1_bias.register_hook(lambda grad: (
-            grad.view(num_head, head_dim).mul_(self.lr_scale[:,None]) +
-            self.fc1_bias.view(num_head, head_dim).mul_(self.weight_decay[:,None]))
-        .view(dim_ff_hidden))
-        self.fc2_weight.register_hook(lambda grad: (
-            grad.view(dim, num_head, head_dim).mul_(self.lr_scale[None,:,None]) +
-            self.fc2_weight.view(dim, num_head, head_dim).mul_(self.weight_decay[None,:,None]))
-        .view(dim, dim_ff_hidden))
+        self.fc1_weight.register_hook(
+            lambda grad: grad.mul_(self.lr_scale[:, None]) + self.fc1_weight.mul_(self.weight_decay[:, None])
+        )
+        self.fc1_bias.register_hook(lambda grad: grad.mul_(self.lr_scale) + self.fc1_bias.mul_(self.weight_decay))
+        self.fc2_weight.register_hook(
+            lambda grad: grad.mul_(self.lr_scale[None, :]) + self.fc2_weight.mul_(self.weight_decay[None, :])
+        )
 
     def forward(self, x: Tensor) -> Tensor:
         x = F.linear(x, self.fc1_weight, self.fc1_bias)
@@ -40,11 +36,12 @@ class MultiLRMLP(nn.Module):
         x = F.linear(x, self.fc2_weight, self.fc2_bias)
         return x
 
+
 class SioConvMultiLRBlock(nn.Module):
-    def __init__(self, dim: int, dim_ff_hidden: int, dropout: float, num_head: int, lr_scale: float, weight_decay: float):
+    def __init__(self, dim: int, dim_ff_hidden: int, dropout: float, lr_scale: float, weight_decay: float):
         super().__init__()
         self.sioconv = SioConvPSLayer(dim)
-        self.ffn = MultiLRMLP(dim, dim_ff_hidden, num_head, lr_scale, weight_decay)
+        self.ffn = MultiLRMLP(dim, dim_ff_hidden, lr_scale, weight_decay)
         self.norm_sioconv = RMSNorm(dim)
         self.norm_ffn = RMSNorm(dim)
         self.dropout = nn.Dropout(dropout)
@@ -64,6 +61,11 @@ class SioConvMultiLRBlock(nn.Module):
 
         return x, hidden
 
+
 class SioConvMultiLR(StackedHiddenState):
-    def __init__(self, depth: int, dim: int, dim_ff_hidden: int, dropout: float, num_head: int, lr_scale: float, weight_decay: float):
-        super().__init__(nn.ModuleList([SioConvMultiLRBlock(dim, dim_ff_hidden, dropout, num_head, lr_scale, weight_decay) for _ in range(depth)]))
+    def __init__(self, depth: int, dim: int, dim_ff_hidden: int, dropout: float, lr_scale: float, weight_decay: float):
+        super().__init__(
+            nn.ModuleList(
+                [SioConvMultiLRBlock(dim, dim_ff_hidden, dropout, lr_scale, weight_decay) for _ in range(depth)]
+            )
+        )
--- tests/models/components/test_sioconv_multi_lr.py
+++ tests/models/components/test_sioconv_multi_lr.py
@@ -9,7 +9,6 @@ DIM = 16
 DIM_FF_HIDDEN = 32
 LEN = 64
 DROPOUT = 0.1
-NUM_HEAD = 4
 LR_SCALE = 100.0
 WEIGHT_DECAY = 0.1
 
@@ -17,7 +16,7 @@ WEIGHT_DECAY = 0.1
 class TestSioConvMultiLR:
     @pytest.fixture
     def sioconv_multi_lr(self):
-        sioconv_multi_lr = SioConvMultiLR(DEPTH, DIM, DIM_FF_HIDDEN, DROPOUT, NUM_HEAD, LR_SCALE, WEIGHT_DECAY)
+        sioconv_multi_lr = SioConvMultiLR(DEPTH, DIM, DIM_FF_HIDDEN, DROPOUT, LR_SCALE, WEIGHT_DECAY)
         return sioconv_multi_lr
 
     def test_sioconv_multi_lr(self, sioconv_multi_lr):
